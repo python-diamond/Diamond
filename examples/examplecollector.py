12558cb45fc847df7fe4525ec27070c1139d033d
Andy Kipp <akipp@brightcove.com>
2011-06-17 13:35:55 -0400

    Fixes to init file Removed old license text from some files

--- examples/examplecollector.py
+++ examples/examplecollector.py
@@ -1,18 +1,22 @@
-# Copyright (C) 2009 Brightcove, Inc. All Rights Reserved. No use,
-# copying or distribution of this work may be made except in
-# accordance with a valid license agreement from Brightcove, Inc. This
-# notice must be included on all copies, modifications and derivatives
-# of this work.
+# Copyright (C) 2010-2011 by Brightcove Inc. 
 #
-# Brightcove, Inc MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE
-# SUITABILITY OF THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING
-# BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT. Brightcove
-# SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT
-# OF USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS
-# DERIVATIVES.
-#
-# "Brightcove" is a trademark of Brightcove, Inc.
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 
 import diamond.collector
 
