f11a9c91a5741d1754dd7e159bf73eed85ffb036
Rob Smith <kormoc@gmail.com>
2012-01-02 18:04:06 -0800

    Manifest updates

--- setup.py
+++ setup.py
@@ -13,19 +13,17 @@ else:
 data_files=[
     ('/etc/diamond',                           glob('conf/*.conf*') ),
     ('/etc/diamond/collectors',                glob('conf/collectors/*') ),
+    ('share/diamond/collectors',               glob('test.py') ),
     ('share/diamond/collectors',               glob('src/collectors/*.py') ),
     ('share/diamond/collectors/user_scripts',  [] ),
-    ('share/diamond/collectors/contrib',       glob('contrib/*.py') ),
 ]
-for path in glob('contrib/*/'):
-    data_files.append(('share/diamond/collectors/'+path, glob(path+'/*')))
 
 setup(
     name            = 'diamond',
     version         = '0.2.0',
     url             = 'https://github.com/BrightcoveOS/Diamond',
     author          = 'The Diamond Team',
-    author_email    = '',
+    author_email    = 'https://github.com/BrightcoveOS/Diamond',
     license         = 'MIT License',
     description     = 'Smart data producer for graphite graphing package',
     package_dir     = {'' : 'src'},
