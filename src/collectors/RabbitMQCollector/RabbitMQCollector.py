b1054cf704556adf782eaa0c8aebde91750aee44
Philip Cristiano <philipcristiano@gmail.com>
2012-01-12 17:57:56 -0500

    RabbitMQCollector: Add collector for RabbitMQ
    The collector will publish aggregate stats for RabbitMQ as well as
    per-queue statistics

---
+++ src/collectors/RabbitMQCollector/RabbitMQCollector.py
@@ -0,0 +1,41 @@
+from numbers import Number
+
+import diamond
+import pyrabbit.api
+
+from pprint import pprint
+
+class RabbitMQCollector(diamond.collector.Collector):
+    """Collects data from RabbitMQ through the admin interface
+
+    """
+
+    def collect(self):
+        client = pyrabbit.api.Client(self.config['host'],
+                                     self.config['user'],
+                                     self.config['password'])
+
+        for queue in client.get_queues():
+            for key in queue:
+                name = '{0}.{1}'.format('queues', queue['name'])
+                self._publish_metrics(name, [], key, queue)
+
+        overview = client.get_overview()
+        for key in overview:
+            self._publish_metrics('', [], key, overview)
+
+    def _publish_metrics(self, name, prev_keys, key, data):
+        """Recursively publish keys"""
+        value = data[key]
+        keys = prev_keys + [key]
+        if isinstance(value, dict):
+            for new_key in value:
+                self._publish_metrics(name, keys, new_key, value)
+        elif isinstance(value, Number):
+            joined_keys = '.'.join(keys)
+            if name:
+                publish_key = '{0}.{1}'.format(name, joined_keys)
+            else:
+                publish_key = joined_keys
+            self.publish(publish_key, value)
+
