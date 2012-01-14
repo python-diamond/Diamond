b78e512f0cd19ba1141daf88211d02731697be8b
Rob Smith <kormoc@gmail.com>
2012-01-13 16:45:27 -0800

    Split the handlers into a directory. I hope this will make the handlers a little bit easier to extend. There is a config file update with the new handler paths.

---
+++ src/diamond/handler/Handler.py
@@ -0,0 +1,25 @@
+import logging
+import threading
+
+class Handler(object):
+    """
+    Handlers process metrics that are collected by Collectors.
+    """
+    def __init__(self, config=None):
+        """
+        Create a new instance of the Handler class
+        """
+        # Initialize Log
+        self.log = logging.getLogger('diamond')
+        # Initialize Data
+        self.config = config
+        # Initialize Lock
+        self.lock = threading.Condition(threading.Lock())
+
+    def process(self, metric):
+        """
+        Process a metric
+
+        Should be overridden in subclasses
+        """
+        raise NotImplementedException
