97debc93859daacd13d2441b286e52d9de02bdb8
Rob Smith <kormoc@gmail.com>
2012-02-28 22:43:50 -0800

    After talking with others, people seem to prefer a simple collector that does not much with the data as much as I had it. Reverting it back to a simple collector.

--- src/collectors/MySQLCollector/MySQLCollector.py
+++ src/collectors/MySQLCollector/MySQLCollector.py
@@ -5,59 +5,6 @@ import re
 
 class MySQLCollector(diamond.collector.Collector):
 
-    REPLACEMENTS = [
-        ('Aborted_',                    'Aborted.'),
-        ('Binlog_cache_',               'Binlog.cache.'),
-        ('Binlog_stmt_',                'Binlog.stmt.'),
-        ('Com_alter_',                  'Com.alter.'),
-        ('Com_create_',                 'Com.create.'),
-        ('Com_drop_',                   'Com.drop.'),
-        ('Com_show_',                   'Com.show.'),
-        ('Com_slave_',                  'Com.slave.'),
-        ('Com_stmt_',                   'Com.stmt.'),
-        ('Com_xa_',                     'Com.xa.'),
-        ('Com_',                        'Com.'),
-        ('Created_tmp_',                'Created_tmp.'),
-        ('Delayed_',                    'Delayed.'),
-        ('Handler_',                    'Handler.'),
-        ('Innodb_adaptive_hash_',       'Innodb.adaptive_hash.'),
-        ('Innodb_buffer_pool_pages_',   'Innodb.buffer_pool.pages.'),
-        ('Innodb_buffer_pool_',         'Innodb.buffer_pool.'),
-        ('Innodb_checkpoint_',          'Innodb.checkpoint.'),
-        ('Innodb_data_',                'Innodb.data.'),
-        ('Innodb_dblwr_',               'Innodb.dblwr.'),
-        ('Innodb_ibuf_',                'Innodb.ibuf.'),
-        ('Innodb_log_',                 'Innodb.log.'),
-        ('Innodb_os_log_',              'Innodb.log.os.'),
-        ('Innodb_lsn_',                 'Innodb.lsn.'),
-        ('Innodb_master_thread_',       'Innodb.master_thread.'),
-        ('Innodb_mem_',                 'Innodb.mem.'),
-        ('Innodb_mutex_',               'Innodb.mutex.'),
-        ('Innodb_pages_',               'Innodb.pages.'),
-        ('Innodb_purge_',               'Innodb.purge.'),
-        ('Innodb_row_lock_',            'Innodb.row_lock.'),
-        ('Innodb_rows_',                'Innodb.rows.'),
-        ('Innodb_s_lock_',              'Innodb.s_lock.'),
-        ('Innodb_x_lock_',              'Innodb.x_lock.'),
-        ('Innodb_',                     'Innodb.'),
-        ('key_blocks_',                 'Key.Buffer.'),
-        ('Key_blocks_',                 'Key.Buffer.'),
-        ('Key_',                        'Key.'),
-        ('Open_',                       'Open.'),
-        ('Opened_',                     'Opened.'),
-        ('Performance_schema_',         'Performance_schema.'),
-        ('Qcache_',                     'Qcache.'),
-        ('Select_',                     'Select.'),
-        ('Slave_',                      'Slave.'),
-        ('Sort_',                       'Sort.'),
-        ('Ssl_',                        'Ssl.'),
-        ('Table_locks_',                'Table_locks.'),
-        ('Tc_log_',                     'Tc_log.'),
-        ('Threads_',                    'Threads.'),
-        ('binlog_',                     'binlog.'),
-        ('max_used_connections',        'Connection.max_used'),
-    ]
-
     def get_default_config(self):
         """
         Returns the default collector settings
@@ -79,7 +26,7 @@ class MySQLCollector(diamond.collector.Collector):
 
     def get_stats(self):
         params = {}
-        stats = {}
+        metrics = {}
 
         params['host']   = self.config['host']
         params['port']   = int(self.config['port'])
@@ -101,66 +48,26 @@ class MySQLCollector(diamond.collector.Collector):
         stats['status'] = dict(cursor.fetchall())
         for key in stats['status']:
             try:
-                stats['status'][key] = float(stats['status'][key])
-            except:
-                pass
-
-        cursor.execute('SHOW GLOBAL VARIABLES')
-        stats['variables'] = dict(cursor.fetchall())
-        for key in stats['variables']:
-            try:
-                stats['variables'][key] = float(stats['variables'][key])
+                metrics[key] = float(stats['status'][key])
             except:
                 pass
 
         db.close()
 
-        return stats
+        return metrics
 
     def collect(self):
-        stats = self.get_stats()
-        metrics = {}
+        metrics = self.get_stats()
 
-        for metric_name in stats['status']:
-            metric_value = stats['status'][metric_name]
+        for metric_name in metrics:
+            metric_value = metrics[metric_name]
+            
             if type(metric_value) is not float:
                 continue
-
-            if 'publish' not in self.config:
-                # Muck the keys to be more graphite friendly
-                for replacement in self.REPLACEMENTS:
-                    metric_name = re.sub('(?i)' + re.escape(replacement[0]), replacement[1], metric_name)
-
-            metric_value = self.derivative(metric_name, metric_value)
-            metrics[metric_name] = metric_value;
-
-        if 'publish' not in self.config:
-            # Build any extra stats here
-
-            # http://themattreid.com/wordpress/2009/04/28/a-quick-rundown-of-per-thread-buffers/
-            metrics['Connection.max_memory_per'] = stats['variables']['read_buffer_size']     \
-                                                 + stats['variables']['read_rnd_buffer_size'] \
-                                                 + stats['variables']['sort_buffer_size']     \
-                                                 + stats['variables']['thread_stack']         \
-                                                 + stats['variables']['join_buffer_size']     \
-                                                 + stats['variables']['binlog_cache_size']
-            metrics['Connection.max_connections'] = stats['variables']['max_connections']
-            metrics['Connection.max_memory'] = metrics['Connection.max_memory_per'] * metrics['Connection.max_connections']
-
-            metrics['Innodb.buffer_pool.total_size'] = stats['variables']['innodb_buffer_pool_size']         \
-                                                     + stats['variables']['innodb_additional_mem_pool_size'] \
-                                                     + stats['variables']['innodb_log_buffer_size']          \
-                                                     + stats['variables']['key_buffer_size']                 \
-                                                     + stats['variables']['query_cache_size']
-            if metrics['Connections'] > 1:
-                metrics['Connection.thread_cache_miss_rate'] = metrics['Threads.created'] / metrics['Connections']
-            metrics['Connection.max'] = stats['variables']['max_connections']
-            metrics['Key_Buffer.unused_bytes'] = metrics['Key.Buffer.unused'] * stats['variables']['key_cache_block_size']
-            metrics['Key_Buffer.used_bytes'] = metrics['Key.Buffer.used'] * stats['variables']['key_cache_block_size']
-
-        if 'publish' not in self.config:
-            for key in metrics:
-                self.publish(key, metrics[key])
+            
+            if 'publish' not in self.config or metric_name in self.config['publish']:
+                metric_value = self.derivative(metric_name, metric_value)
+                self.publish(metric_name, metric_value)
         else:
             for k in self.config['publish'].split():
                 if not metrics.has_key(k):
