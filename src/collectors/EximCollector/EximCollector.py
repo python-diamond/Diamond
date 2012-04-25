5a28e67dd9070d77da75e109d3ea1d63c84e4347
Rob Smith <kormoc@gmail.com>
2012-04-25 14:22:27 -0700

    Fix whitespace on the EximCollector and add in get_default_config

--- src/collectors/EximCollector/EximCollector.py
+++ src/collectors/EximCollector/EximCollector.py
@@ -4,14 +4,22 @@ import subprocess
 import os
 
 class EximCollector(diamond.collector.Collector):
-  """
-  Shells out to get the exim queue length
-  """
+    """
+    Shells out to get the exim queue length
+    """
+  
+    COMMAND = ['/usr/sbin/exim', '-bpc']
+    
+    def get_default_config(self):
+        """
+        Returns the default collector settings
+        """
+        return {
+            'path' : 'exim',
+        }
 
-  COMMAND = ['/usr/sbin/exim', '-bpc']
-
-  def collect(self):
-    if not os.access(EximCollector.COMMAND[0], os.X_OK):
-      return
-    queuesize = subprocess.Popen(EximCollector.COMMAND, stdout=subprocess.PIPE).communicate()[0][:-1]
-    self.publish('queuesize', queuesize)
+    def collect(self):
+        if not os.access(EximCollector.COMMAND[0], os.X_OK):
+            return
+        queuesize = subprocess.Popen(EximCollector.COMMAND, stdout=subprocess.PIPE).communicate()[0][:-1]
+        self.publish('queuesize', queuesize)
