0eb99264c468cd5b35159bb18c5d6be0d385d060
Rob Smith <kormoc@gmail.com>
2012-06-08 17:13:07 -0700

    Support the new test location per #76

--- test.py
+++ test.py
@@ -109,6 +109,7 @@ def getCollectorTests(path):
 
         if os.path.isfile(cPath) and len(f) > 3 and f[-3:] == '.py' and f[0:4] == 'Test':
             sys.path.append(os.path.dirname(cPath))
+            sys.path.append(os.path.dirname(os.path.dirname(cPath)))
             modname = f[:-3]
             try:
                 # Import the module
