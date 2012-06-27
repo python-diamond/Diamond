78e4a46ace83c4d760c2646bf6d91f7cd3047f41
Rob Smith <kormoc@gmail.com>
2012-06-27 14:08:43 -0700

    Add in TestSmartCollector and fix a edgecase in SmartCollector

--- src/collectors/smart/smart.py
+++ src/collectors/smart/smart.py
@@ -14,6 +14,7 @@ class SmartCollector(diamond.collector.Collector):
         """
         return {
             'path': 'smart',
+            'bin' : 'smartctl',
             'devices': '^disk[0-9]$|^sd[a-z]$|^hd[a-z]$',
             'method': 'Threaded'
         }
@@ -26,12 +27,29 @@ class SmartCollector(diamond.collector.Collector):
 
         for device in os.listdir('/dev'):
             if devices.match(device):
-                attributes = subprocess.Popen(["smartctl", "-A", os.path.join('/dev',device)],
+                attributes = subprocess.Popen([self.config['bin'], "-A", os.path.join('/dev',device)],
                              stdout=subprocess.PIPE).communicate()[0].strip().splitlines()
+                
+                metrics = {}
 
                 for attr in attributes[7:]:
                     attribute = attr.split()
                     if attribute[1] != "Unknown_Attribute":
-                        self.publish("%s.%s" % (device, attribute[1]), attribute[9])
+                        metric = "%s.%s" % (device, attribute[1])
                     else:
-                        self.publish("%s.%s" % (device, attribute[0]), attribute[9])
+                        metric = "%s.%s" % (device, attribute[0])
+                        
+                    # New metric? Store it
+                    if not metrics.has_key(metric):
+                        metrics[metric] = attribute[9]
+                    # Duplicate metric? Only store if it has a larger value
+                    # This happens semi-often with the Temperature_Celsius attribute
+                    # You will have a PASS/FAIL after the real temp, so only overwrite if
+                    # The earlier one was a PASS/FAIL (0/1)
+                    elif metrics[metric] == 0 and attribute[9] > 0:
+                        metrics[metric] = attribute[9]
+                    else:
+                        continue
+                        
+                for metric in metrics.keys():
+                    self.publish(metric, metrics[metric])
