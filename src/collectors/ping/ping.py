e2c1cce53f5728e1a872a4d92440717eb31ee1af
Rob Smith <kormoc@gmail.com>
2012-06-27 11:57:22 -0700

    Add in TestPingCollector and fix PingCollector to work with OS X

--- src/collectors/ping/ping.py
+++ src/collectors/ping/ping.py
@@ -22,9 +22,17 @@ class PingCollector(diamond.collector.Collector):
                 metric_name = host.replace('.','_');
 
                 ping = subprocess.Popen(["ping", '-nq', '-c 1', host], stdout=subprocess.PIPE).communicate()[0].strip().split("\n")[-1]
-                if ping[0:3] != 'rtt':
-                    metric_value = 10000
-                else :
+                
+                # Linux
+                if ping.startswith('rtt'):
                     ping = ping.split()[3].split('/')[0]
                     metric_value = int(round(float(ping)))
+                # OS X
+                elif ping.startswith('round-trip '):
+                    ping = ping.split()[3].split('/')[0]
+                    metric_value = int(round(float(ping)))
+                # Unknown
+                else :
+                    metric_value = 10000
+                    
                 self.publish(metric_name, metric_value)
---
+++ src/collectors/ping/test/testping.py
@@ -0,0 +1,130 @@
+#!/usr/bin/python
+################################################################################
+
+from test import *
+
+from diamond.collector import Collector
+from ping import PingCollector
+
+################################################################################
+
+class TestPingCollector(CollectorTestCase):
+    def setUp(self):
+        config = get_collector_config('PingCollector', {
+            'interval': 10,
+            'target_a': 'localhost'
+        })
+
+        self.collector = PingCollector(config, None)
+
+    @patch('os.access', Mock(return_value=True))
+    @patch.object(Collector, 'publish')
+    def test_should_work_with_real_data_bad_gentoo(self, publish_mock):
+        with patch('subprocess.Popen.communicate', Mock(return_value =
+            ( self.getFixture('bad_gentoo').getvalue() , '')
+        )):
+            self.collector.collect()
+
+        self.assertPublishedMany(publish_mock, {
+            'localhost': 10000
+        })
+
+    @patch('os.access', Mock(return_value=True))
+    @patch.object(Collector, 'publish')
+    def test_should_work_with_real_data_host_gentoo(self, publish_mock):
+        with patch('subprocess.Popen.communicate', Mock(return_value =
+            ( self.getFixture('host_gentoo').getvalue() , '')
+        )):
+            self.collector.collect()
+
+        self.assertPublishedMany(publish_mock, {
+            'localhost': 11
+        })
+        
+    @patch('os.access', Mock(return_value=True))
+    @patch.object(Collector, 'publish')
+    def test_should_work_with_real_data_ip_gentoo(self, publish_mock):
+        with patch('subprocess.Popen.communicate', Mock(return_value =
+            ( self.getFixture('ip_gentoo').getvalue() , '')
+        )):
+            self.collector.collect()
+
+        self.assertPublishedMany(publish_mock, {
+            'localhost': 0
+        })
+
+    @patch('os.access', Mock(return_value=True))
+    @patch.object(Collector, 'publish')
+    def test_should_work_with_real_data_longhost_gentoo(self, publish_mock):
+        with patch('subprocess.Popen.communicate', Mock(return_value =
+            ( self.getFixture('longhost_gentoo').getvalue() , '')
+        )):
+            self.collector.collect()
+
+        self.assertPublishedMany(publish_mock, {
+            'localhost': 10
+        })
+
+    @patch('os.access', Mock(return_value=True))
+    @patch.object(Collector, 'publish')
+    def test_should_work_with_real_data_timeout_gentoo(self, publish_mock):
+        with patch('subprocess.Popen.communicate', Mock(return_value =
+            ( self.getFixture('timeout_gentoo').getvalue() , '')
+        )):
+            self.collector.collect()
+
+        self.assertPublishedMany(publish_mock, {
+            'localhost': 10000
+        })
+
+    @patch('os.access', Mock(return_value=True))
+    @patch.object(Collector, 'publish')
+    def test_should_work_with_real_data_host_osx(self, publish_mock):
+        with patch('subprocess.Popen.communicate', Mock(return_value =
+            ( self.getFixture('host_osx').getvalue() , '')
+        )):
+            self.collector.collect()
+
+        self.assertPublishedMany(publish_mock, {
+            'localhost': 38
+        })
+        
+    @patch('os.access', Mock(return_value=True))
+    @patch.object(Collector, 'publish')
+    def test_should_work_with_real_data_ip_osx(self, publish_mock):
+        with patch('subprocess.Popen.communicate', Mock(return_value =
+            ( self.getFixture('ip_osx').getvalue() , '')
+        )):
+            self.collector.collect()
+
+        self.assertPublishedMany(publish_mock, {
+            'localhost': 0
+        })
+
+    @patch('os.access', Mock(return_value=True))
+    @patch.object(Collector, 'publish')
+    def test_should_work_with_real_data_longhost_osx(self, publish_mock):
+        with patch('subprocess.Popen.communicate', Mock(return_value =
+            ( self.getFixture('longhost_osx').getvalue() , '')
+        )):
+            self.collector.collect()
+
+        self.assertPublishedMany(publish_mock, {
+            'localhost': 42
+        })
+
+    @patch('os.access', Mock(return_value=True))
+    @patch.object(Collector, 'publish')
+    def test_should_work_with_real_data_timeout_osx(self, publish_mock):
+        with patch('subprocess.Popen.communicate', Mock(return_value =
+            ( self.getFixture('timeout_osx').getvalue() , '')
+        )):
+            self.collector.collect()
+
+        self.assertPublishedMany(publish_mock, {
+            'localhost': 10000
+        })
+
+################################################################################
+if __name__ == "__main__":
+    unittest.main()
