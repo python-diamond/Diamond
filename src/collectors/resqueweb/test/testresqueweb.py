9b284c57bd28cc3eee3c26135e8952b552a9556a
Mike Zupan <mike@zcentric.com>
2012-07-02 16:35:34 -0700

    fixed the test

--- src/collectors/resqueweb/test/testresqueweb.py
+++ src/collectors/resqueweb/test/testresqueweb.py
@@ -6,36 +6,20 @@ from test import *
 from diamond.collector import Collector
 from resqueweb import ResqueWebCollector
 
-import resqueweb
-import urllib2
-
 ################################################################################
-
-class RequestStub(object):
-    def __init__(self, data):
-        self.data = data
-
-    def read(self):
-        return self.data
 
 class TestResqueWebCollector(CollectorTestCase):
     def setUp(self):
-        config = get_collector_config('resqueweb.ResqueWebCollector', {
+        config = get_collector_config('ResqueWebCollector', {
             'interval': 10
         })
 
         self.collector = ResqueWebCollector(config, None)
 
-    def urlopen_stub(self, *args, **kwargs):
-        return RequestStub(self.getFixture('stats.txt').getvalue())
-
-    def urlopen_stub_null(self, *args, **kwargs):
-        return RequestStub('')
-
     @patch.object(Collector, 'publish')
     def test_should_work_with_real_data(self, publish_mock):
-        resqueweb.urllib2.urlopen = self.urlopen_stub
-        self.collector.collect()
+        with patch('urllib2.urlopen', Mock(return_value = self.getFixture('stats.txt'))):
+            self.collector.collect()
             
         self.assertPublishedMany(publish_mock, {
             'pending.current' : 2, 
@@ -51,8 +35,8 @@ class TestResqueWebCollector(CollectorTestCase):
 
     @patch.object(Collector, 'publish')
     def test_should_fail_gracefully(self, publish_mock):
-        resqueweb.urllib2.urlopen = self.urlopen_stub_null
-        self.collector.collect()
+        with patch('urllib2.urlopen', Mock(return_value = self.getFixture('stats_blank.txt'))):
+            self.collector.collect()
             
         self.assertPublishedMany(publish_mock, {})
 
