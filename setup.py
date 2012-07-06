349d69141b7ae6a8159f5ac4df95a97669a9a05e
Rob Smith <kormoc@gmail.com>
2012-07-06 10:40:43 -0700

    Fixes #112, this adds a .keep file to /var/log/diamond to ensure the path is created as part of the rpm

--- setup.py
+++ setup.py
@@ -26,7 +26,8 @@ else:
     if platform.dist()[0] == 'Ubuntu':
         data_files.append(('/etc/init',                      ['debian/upstart/diamond.conf'] ))
     if platform.dist()[0] == 'centos' or 'redhat':
-        data_files.append(('/etc/init.d',		     ['bin/init.d/diamond'] ))
+        data_files.append(('/etc/init.d',		             ['bin/init.d/diamond'] ))
+        data_files.append(('/var/log/diamond',		         ['.keep'] ))
 
 def pkgPath(root, path, rpath="/"):
     global data_files
