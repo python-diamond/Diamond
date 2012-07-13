1120efb0005ef9aced500ba9809eddb827c045fc
Rob Smith <kormoc@gmail.com>
2012-07-13 00:22:56 -0700

    Config should ignore test* not Test*

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ def getCollectors(path):
     for f in os.listdir(path):
         cPath = os.path.abspath(os.path.join(path, f))
 
-        if os.path.isfile(cPath) and len(f) > 3 and f[-3:] == '.py' and f[0:4] != 'Test':
+        if os.path.isfile(cPath) and len(f) > 3 and f[-3:] == '.py' and f[0:4] != 'test':
             sys.path.append(os.path.dirname(cPath))
             modname = f[:-3]
             try:
