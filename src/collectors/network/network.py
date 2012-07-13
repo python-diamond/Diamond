bac98ad1c5aaf24f463189b56327ac3aaaf8d342
Rob Smith <kormoc@gmail.com>
2012-07-13 11:40:47 -0700

    Fixes #114, this converts a python 2.6+ specific code to python 2.4 compatable syntax

--- src/collectors/network/network.py
+++ src/collectors/network/network.py
@@ -75,7 +75,10 @@ class NetworkCollector(diamond.collector.Collector):
             # Open File
             file = open(self.PROC)
             # Build Regular Expression
-            greed = '0-9' if self.config['greedy'].lower == 'true' else ''
+            greed = ''
+            if self.config['greedy'].lower == 'true' :
+                greed = '0-9'
+            
             exp = '^(?:\s*)([%s%s]+):(?:\s*)(?P<rx_bytes>\d+)(?:\s*)(?P<rx_packets>\w+)(?:\s*)(?P<rx_errors>\d+)(?:\s*)(?P<rx_drop>\d+)(?:\s*)(?P<rx_fifo>\d+)(?:\s*)(?P<rx_frame>\d+)(?:\s*)(?P<rx_compressed>\d+)(?:\s*)(?P<rx_multicast>\d+)(?:\s*)(?P<tx_bytes>\d+)(?:\s*)(?P<tx_packets>\w+)(?:\s*)(?P<tx_errors>\d+)(?:\s*)(?P<tx_drop>\d+)(?:\s*)(?P<tx_fifo>\d+)(?:\s*)(?P<tx_frame>\d+)(?:\s*)(?P<tx_compressed>\d+)(?:\s*)(?P<tx_multicast>\d+)(?:.*)$' % (( '|'.join(self.config['interfaces']) ), greed)
             reg = re.compile(exp)
             # Match Interfaces
