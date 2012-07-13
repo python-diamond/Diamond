24598abe19fc8f4e0624312ca2f1e0d8d20d0c52
Rob Smith <kormoc@gmail.com>
2012-07-13 12:03:03 -0700

    Fixes #115, this converts a python 2.5+ codepath to 2.4+

--- src/collectors/hadoop/hadoop.py
+++ src/collectors/hadoop/hadoop.py
@@ -48,64 +48,65 @@ class HadoopCollector(diamond.collector.Collector):
             self.log.error('HadoopCollector unable to read "%s"' % (filename,))
             return False
 
-        with open(filename, 'r') as fd:
-            for line in fd:
-                match = self.re_log.match(line)
-                if not match:
-                    continue
-                
-                metrics={}
+        fd = open(filename, 'r')
+        for line in fd:
+            match = self.re_log.match(line)
+            if not match:
+                continue
+            
+            metrics={}
 
-                data = match.groupdict()
-                for metric in data['metrics'].split(','):
-                    metric = metric.strip()
-                    if '=' in metric:
-                        key, value = metric.split('=', 1)
-                        metrics[key]=value
+            data = match.groupdict()
+            for metric in data['metrics'].split(','):
+                metric = metric.strip()
+                if '=' in metric:
+                    key, value = metric.split('=', 1)
+                    metrics[key]=value
 
-                for metric in metrics.keys():
-                    try:
+            for metric in metrics.keys():
+                try:
+                    
+                    if data['name'] == 'jvm.metrics':
+                        path = self.get_metric_path('.'.join([
+                            data['name'],
+                            metrics['hostName'].replace('.', '_'),
+                            metrics['processName'].replace(' ', '_'),
+                            metric,
+                        ]))
                         
-                        if data['name'] == 'jvm.metrics':
-                            path = self.get_metric_path('.'.join([
-                                data['name'],
-                                metrics['hostName'].replace('.', '_'),
-                                metrics['processName'].replace(' ', '_'),
-                                metric,
-                            ]))
-                            
-                        elif data['name'] == 'mapred.job':
-                            path = self.get_metric_path('.'.join([
-                                data['name'],
-                                metrics['hostName'].replace('.', '_'),
-                                metrics['group'].replace(' ', '_'),
-                                metrics['counter'].replace(' ', '_'),
-                                metric,
-                            ]))
-                            
-                        elif data['name'] == 'rpc.metrics':
-                            
-                            if metric == 'port':
-                                continue
-                            
-                            path = self.get_metric_path('.'.join([
-                                data['name'],
-                                metrics['hostName'].replace('.', '_'),
-                                metrics['port'],
-                                metric,
-                            ]))
+                    elif data['name'] == 'mapred.job':
+                        path = self.get_metric_path('.'.join([
+                            data['name'],
+                            metrics['hostName'].replace('.', '_'),
+                            metrics['group'].replace(' ', '_'),
+                            metrics['counter'].replace(' ', '_'),
+                            metric,
+                        ]))
                         
-                        else :
-                            path = self.get_metric_path('.'.join([
-                                data['name'],
-                                metric,
-                            ]))
+                    elif data['name'] == 'rpc.metrics':
                         
-                        value = float(metrics[metric])
+                        if metric == 'port':
+                            continue
                         
-                        self.publish_metric(Metric(path,
-                            value,
-                            timestamp=int(data['timestamp'])))
+                        path = self.get_metric_path('.'.join([
+                            data['name'],
+                            metrics['hostName'].replace('.', '_'),
+                            metrics['port'],
+                            metric,
+                        ]))
+                    
+                    else :
+                        path = self.get_metric_path('.'.join([
+                            data['name'],
+                            metric,
+                        ]))
+                    
+                    value = float(metrics[metric])
+                    
+                    self.publish_metric(Metric(path,
+                        value,
+                        timestamp=int(data['timestamp'])))
 
-                    except (ValueError):
-                        pass
+                except (ValueError):
+                    pass
+        fd.close()
