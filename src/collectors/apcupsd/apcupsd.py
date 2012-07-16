42bab3e71149e94d439d4bb876c2fc596c9ef329
Rob Smith <kormoc@gmail.com>
2012-07-16 13:08:40 -0700

    Add in a apcupsd collector

---
+++ src/collectors/apcupsd/apcupsd.py
@@ -0,0 +1,97 @@
+"""
+Collects the complete status of most American Power Conversion Corp. (APC) UPSes
+provided you have the apcupsd daemon installed, properly configured and
+running. It can access status information from any APC UPS attached to the
+localhost or attached to any computer on the network which is running
+apcuspd in NIS mode.
+
+#### Dependencies
+
+ * apcuspd in NIS mode
+
+"""
+
+import diamond.collector
+import socket
+from struct import *
+import re
+
+class ApcupsdCollector(diamond.collector.Collector):
+    
+    def get_default_config_help(self):
+        config_help = super(ApcupsdCollector, self).get_default_config_help()
+        config_help.update({
+            'hostname' : 'Hostname to collect from',
+            'port' : 'port to collect from. defaults to 3551',
+            'metrics' : 'List of metrics. Valid metric keys can be found at (http://www.apcupsd.com/manual/manual.html#status-report-fields)'
+        })
+        return config_help
+    
+    def get_default_config(self):
+        """
+        Returns the default collector settings
+        """
+        config = super(ApcupsdCollector, self).get_default_config()
+        config.update( {
+            'enabled':  'True',
+            'path':     'apcupsd',
+            'hostname': 'localhost',
+            'port': 3551,
+            'metrics' : ['LINEV', 'LOADPCT', 'BCHARGE', 'TIMELEFT', 'BATTV',
+                         'NUMXFERS', 'TONBATT', 'MAXLINEV', 'MINLINEV',
+                         'OUTPUTV', 'ITEMP', 'LINEFREQ', 'CUMONBATT', ],
+        } )
+        return config
+    
+    def getData(self):
+        # Get the data via TCP stream
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((self.config['hostname'], int(self.config['port'])))
+        
+        # Packet is pad byte, size byte, and command
+        s.send(pack('xb6s', 6, 'status'))
+        
+        # Ditch the header
+        s.recv(1024)
+        data = s.recv(1024)
+        
+        # We're done. Close the socket
+        s.close()
+        return data
+        
+
+    def collect(self):
+        metrics = {}
+        raw = {}
+        
+        data = self.getData()
+        
+        data = data.split('\n\x00')
+        for d in data:
+            matches = re.search("([A-Z]+)\s+:\s+(.*)$", d)
+            if matches:
+                value = matches.group(2).strip()
+                raw[matches.group(1)] = matches.group(2).strip()
+                vmatch = re.search("([0-9.]+)", value)
+                if not vmatch:
+                    continue
+                try:
+                    value = float(vmatch.group(1))
+                except:
+                    continue
+                metrics[matches.group(1)] = value
+                
+        for metric in self.config['metrics']:
+            if not metrics.has_key(metric):
+                continue
+            
+            metric_name = "%s.%s" % (raw['UPSNAME'], metric)
+            
+            value = metrics[metric]
+            
+            if metric in ['TONBATT', 'CUMONBATT', 'NUMXFERS']:
+                value = self.derivative(metric_name, metrics[metric])
+                
+            self.publish(metric_name, value)
+        
+        return True
---
+++ src/collectors/apcupsd/test/testapcupsd.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+################################################################################
+
+from test import *
+
+from diamond.collector import Collector
+from apcupsd import ApcupsdCollector
+
+################################################################################
+
+class TestApcupsdCollector(CollectorTestCase):
+    def setUp(self):
+        config = get_collector_config('ApcupsdCollector', {
+            'interval': 10
+        })
+
+        self.collector = ApcupsdCollector(config, None)
+
+    @patch.object(Collector, 'publish')
+    def test_should_work_with_synthetic_data(self, publish_mock):
+        with patch.object(ApcupsdCollector, 'getData', Mock(return_value = 'APC      : 001,039,1056\n\x00\'DATE     : 2012-07-16 12:53:58 -0700  \n\x00 HOSTNAME : localhost\n\x00+VERSION  : 3.14.8 (16 January 2010) redhat\n\x00 UPSNAME  : localhost\n\x00\x15CABLE    : USB Cable\n\x00\x1dMODEL    : Back-UPS BX1300G \n\x00\x17UPSMODE  : Stand Alone\n\x00\'STARTTIME: 2011-12-07 10:28:24 -0800  \n\x00\x13STATUS   : ONLINE \n\x00\x17LINEV    : 124.0 Volts\n\x00\'LOADPCT  :   5.0 Percent Load Capacity\n\x00\x19BCHARGE  : 100.0 Percent\n\x00\x19TIMELEFT :  73.9 Minutes\n\x00\x15MBATTCHG : 5 Percent\n\x00\x15MINTIMEL : 3 Minutes\n\x00\x15MAXTIME  : 0 Seconds\n\x00\x12SENSE    : Medium\n\x00\x17LOTRANS  : 088.0 Volts\n\x00\x17HITRANS  : 139.0 Volts\n\x00\x12ALARMDEL : Always\n\x00\x16BATTV    : 27.3 Volts\n\x00+LASTXFER : Automatic or explicit self test\n\x00\x0eNUMXFERS : 19\n\x00\'XONBATT  : 2012-07-13 09:11:52 -0700  \n\x00\x15TONBATT  : 0 seconds\n\x00\x17CUMONBATT: 130 seconds\n\x00\'XOFFBATT : 2012-07-13 09:12:01 -0700  \n\x00\'LASTSTEST: 2012-07-13 09:11:52 -0700  \n\x00\x0eSELFTEST : NO\n\x00"STATFLAG : 0x07000008 Status Flag\n\x00\x16MANDATE  : 2009-10-08\n\x00\x1aSERIALNO : 3B0941X40219  \n\x00\x16BATTDATE : 2009-10-08\n\x00\x15NOMINV   : 120 Volts\n\x00\x17NOMBATTV :  24.0 ')):
+            self.collector.collect()
+
+        self.assertPublishedMany(publish_mock, {
+            'localhost.LINEV': 124.000000, 
+            'localhost.LOADPCT': 5.000000, 
+            'localhost.BCHARGE': 100.000000, 
+            'localhost.TIMELEFT': 73.900000, 
+            'localhost.BATTV': 27.300000, 
+            'localhost.NUMXFERS': 0.000000, 
+            'localhost.TONBATT': 0.000000, 
+        })
+
+################################################################################
+if __name__ == "__main__":
+    unittest.main()
