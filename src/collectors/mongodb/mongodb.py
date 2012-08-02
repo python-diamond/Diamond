python-diamond/Diamond

commit def20ad90edbcaba3e063445c8a7f01ab8ca5a46

making mongodb collector python 2.4 compatible

src/collectors/mongodb/mongodb.py
@@ -11,7 +11,6 @@ values are ignored.
 import diamond.collector
 
 try:
-    from numbers import Number
     import pymongo
     from pymongo import ReadPreference
 except ImportError:
@@ -60,5 +59,5 @@ class MongoDBCollector(diamond.collector.Collector):
         if isinstance(value, dict):
             for new_key in value:
                 self._publish_metrics(keys, new_key, value)
-        elif isinstance(value, Number):
+        elif isinstance(value, int) or isinstance(value, float):
             self.publish('.'.join(keys), value)

commit 0c49f3e7d1c22d4b54038b5a26e84ce9b1c10b1a

Re #124, this fixes an bug introduced by def20ad90edbcaba3e063445c8a7f01ab8ca5a46 with the detection of failed imports

src/collectors/mongodb/mongodb.py
@@ -14,7 +14,7 @@ try:
     import pymongo
     from pymongo import ReadPreference
 except ImportError:
-    Number = None
+    pymongo = None
 
 class MongoDBCollector(diamond.collector.Collector):
     
@@ -39,8 +39,8 @@ class MongoDBCollector(diamond.collector.Collector):
     def collect(self):
         """Collect number values from db.serverStatus()"""
 
-        if Number is None:
-            self.log.error('Unable to import either Number or pymongo')
+        if pymongo is None:
+            self.log.error('Unable to import pymongo')
             return {}
 
         try: