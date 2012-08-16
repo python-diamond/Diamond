a233f685f6cb514420fd534388d51ee92459d886
Rob Smith <kormoc@gmail.com>
2012-08-15 23:08:33 -0700

    Remove duplicate imports and remove entirly unused string

--- src/diamond/__init__.py
+++ src/diamond/__init__.py
@@ -1,28 +1,22 @@
 # coding=utf-8
 
+"""
+Diamond module init code
+"""
+
 import os
 import sys
-import string
 import logging
 import time
 import traceback
 import configobj
 import socket
 import re
-import os
-import sys
-import re
-import logging
-import time
 import datetime
 import random
 import urllib2
 import base64
 import csv
 import platform
-import string
-import traceback
-import configobj
-import socket
 
 from urlparse import urlparse
