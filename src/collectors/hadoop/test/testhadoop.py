688eb257160a6e72b8738b28dc39d68107f67cbd
Rob Smith <kormoc@gmail.com>
2012-08-17 16:21:19 -0700

    Empty line includes whitespace

--- src/collectors/hadoop/test/testhadoop.py
+++ src/collectors/hadoop/test/testhadoop.py
@@ -23,9 +23,9 @@ class TestHadoopCollector(CollectorTestCase):
     @patch.object(Collector, 'publish_metric')
     def test_should_work_with_real_data(self, publish_mock):
         self.collector.collect()
-        
+
         metrics = self.getPickledResults('expected.pkl')
-        
+
         self.setDocExample(self.collector.__class__.__name__, metrics)
         self.assertPublishedMetricMany(publish_mock, metrics)
 
