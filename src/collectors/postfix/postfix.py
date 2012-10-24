f5acef1fb1a237bbb98f97fcdbb2b69110bf364d
Rob Smith <kormoc@gmail.com>
2012-10-24 14:06:09 -0700

    Add in a postfix-stats collector

---
+++ src/collectors/postfix/postfix.py
@@ -0,0 +1,77 @@
+# coding=utf-8
+
+"""
+Collect stats from postfix-stats. postfix-stats is a simple threaded stats
+aggregator for Postfix. When running as a syslog destination, it can be used to
+get realtime cumulative stats.
+
+#### Dependencies
+
+ * json
+ * socket
+ * StringIO
+ * [postfix-stats](https://github.com/disqus/postfix-stats)
+
+"""
+
+import diamond.collector
+import socket
+from StringIO import StringIO
+
+try:
+    import json
+except ImportError:
+    import simplejson as json
+    
+class PostfixCollector(diamond.collector.Collector):
+
+    def get_default_config_help(self):
+        config_help = super(PostfixCollector, self).get_default_config_help()
+        config_help.update({
+            'host': 'Hostname to coonect to',
+            'port': 'Port to connect to',
+        })
+        return config_help
+
+    def get_default_config(self):
+        """
+        Returns the default collector settings
+        """
+        config = super(PostfixCollector, self).get_default_config()
+        config.update({
+            'path':     'postfix',
+            'host':     'localhost',
+            'port':     7777,
+        })
+        return config
+    
+    def getJson(self):
+        sock = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+        sock.connect( (self.config['host'], self.config['port']) )
+        sock.send("stats\n")
+        jsondata = sock.recv(4096)
+        sock.close()
+        return jsondata
+    
+    def getData(self):
+        jsondata = self.getJson()
+        io = StringIO(jsondata)
+        data = json.load(io)
+        return data
+
+    def collect(self):
+        data = self.getData()
+        
+        for nodetype in data.keys():
+            for nodesubtype in data[nodetype].keys():
+                for metric in data[nodetype][nodesubtype].keys():
+                    
+                    # End metrics are sometimes codes like 2.0.0
+                    metricname = metric.replace('.', '_')
+                    
+                    metric_name = "%s.%s.%s" % (nodetype, nodesubtype, metricname)
+                    metric_value = data[nodetype][nodesubtype][metric]
+                    
+                    metric_value = self.derivative(metric_name, metric_value)
+        
+                    self.publish(metric_name, metric_value)
---
+++ src/collectors/postfix/test/testpostfix.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+# coding=utf-8
+################################################################################
+
+from test import CollectorTestCase
+from test import get_collector_config
+from test import unittest
+from mock import Mock
+from mock import patch
+
+from diamond.collector import Collector
+from postfix import PostfixCollector
+
+################################################################################
+
+
+
+class TestPostfixCollector(CollectorTestCase):
+    def setUp(self):
+        config = get_collector_config('PostfixCollector', {
+            'host':     'localhost',
+            'port':     7777,
+            'interval': '1',
+        })
+
+        self.collector = PostfixCollector(config, None)
+        
+    @patch.object(Collector, 'publish')
+    def test_should_work_with_synthetic_data(self, publish_mock):
+        with patch.object(PostfixCollector,
+                          'getJson',
+                          Mock(return_value='{"local": {}, "clients": {}, "recv": {"status": {}, "resp_codes": {}}, "send": {"status": {"sent": 0}, "resp_codes": {"2.0.0": 0}}, "in": {"status": {}, "resp_codes": {}}}')):
+            self.collector.collect()
+            
+        self.assertPublishedMany(publish_mock, {})
+
+        with patch.object(PostfixCollector,
+                          'getJson',
+                          Mock(return_value='{"local": {}, "clients": {}, "recv": {"status": {}, "resp_codes": {}}, "send": {"status": {"sent": 4}, "resp_codes": {"2.0.0": 5}}, "in": {"status": {}, "resp_codes": {}}}')):
+            self.collector.collect()
+
+        self.assertPublishedMany(publish_mock, {
+            'send.status.sent': 4,
+            'send.resp_codes.2_0_0': 5,
+        })
+
+################################################################################
+if __name__ == "__main__":
+    unittest.main()
