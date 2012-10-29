46bcab267d9ae2e3f24dd5a72e87ace6d1c9b690
Tomas Neme <lacrymology@gmail.com>
2012-10-29 17:00:55 -0300

    config methods

--- src/collectors/processmemory/processmemory.py
+++ src/collectors/processmemory/processmemory.py
@@ -16,3 +16,30 @@ name=^postgres,^pg
 
 exe and name are both lists of comma-separated regexps.
 """
+import re
+
+import diamond.collector
+import psutil
+
+class ProcessMemoryCollector(diamond.collector.Collector):
+
+    def get_default_config_help(self):
+        config_help = super(ProcessMemoryCollector,
+                            self).get_default_config_help()
+        config_help.update({
+            'unit': ('The unit in which memory data is collected.'
+                     'Can be one of b, kb, mb, gb'),
+            'process': ("A subcategory of settings inside of which each "
+                        "collected process has it's configuration")
+        })
+        return config_help
+
+    def get_default_config(self):
+        config = super(ProcessMemoryCollector, self).get_default_config()
+        config.update({
+            'path': 'memory.process',
+            'unit': 'b',
+            'process': '',
+            })
+        return config
+
