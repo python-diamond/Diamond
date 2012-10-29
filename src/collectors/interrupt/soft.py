8d96703b190790f1e348c22465f3604ee7882e31
Rob Smith <kormoc@gmail.com>
2012-10-28 21:35:21 -0700

    Add in a soft irq collector

---
+++ src/collectors/interrupt/soft.py
@@ -0,0 +1,80 @@
+# coding=utf-8
+
+"""
+The SoftInterruptCollector collects metrics on software interrupts from
+/proc/stat
+
+#### Dependencies
+
+ * /proc/stat
+
+"""
+
+import platform
+import os
+import diamond.collector
+
+# Detect the architecture of the system
+# and set the counters for MAX_VALUES
+# appropriately. Otherwise, rolling over
+# counters will cause incorrect or
+# negative values.
+if platform.architecture()[0] == '64bit':
+    counter = (2 ** 64) - 1
+else:
+    counter = (2 ** 32) - 1
+
+
+class SoftInterruptCollector(diamond.collector.Collector):
+
+    PROC = '/proc/stat'
+
+    def get_default_config_help(self):
+        config_help = super(SoftInterruptCollector, self).get_default_config_help()
+        config_help.update({
+        })
+        return config_help
+
+    def get_default_config(self):
+        """
+        Returns the default collector settings
+        """
+        config = super(SoftInterruptCollector, self).get_default_config()
+        config.update({
+            'path':     'softirq'
+        })
+        return config
+
+    def collect(self):
+        """
+        Collect interrupt data
+        """
+        if not os.access(self.PROC, os.R_OK):
+            return False
+
+        #Open PROC file
+        file = open(self.PROC, 'r')
+        
+        #Get data
+        for line in file:
+            
+            if not line.startswith('softirq'):
+                continue
+            
+            data = line.split()
+            
+            metric_name = 'total'
+            metric_value = int(data[1])
+            metric_value = int(self.derivative(metric_name,
+                                    long(metric_value), counter))
+            self.publish(metric_name, metric_value)
+            
+            for i in range(2, len(data)):
+                metric_name = str(i-2)
+                metric_value = int(data[i])
+                metric_value = int(self.derivative(metric_name,
+                                        long(metric_value), counter))
+                self.publish(metric_name, metric_value)
+
+        #Close file
+        file.close()
---
+++ src/collectors/interrupt/test/testsoft.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python
+# coding=utf-8
+################################################################################
+
+from test import CollectorTestCase
+from test import get_collector_config
+from test import unittest
+from mock import Mock
+from mock import patch
+
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from StringIO import StringIO
+
+from diamond.collector import Collector
+from soft import SoftInterruptCollector
+
+################################################################################
+
+
+class TestSoftInterruptCollector(CollectorTestCase):
+    def setUp(self):
+        config = get_collector_config('SoftInterruptCollector', {
+            'interval': 1
+        })
+
+        self.collector = SoftInterruptCollector(config, None)
+
+    @patch('__builtin__.open')
+    @patch('os.access', Mock(return_value=True))
+    @patch.object(Collector, 'publish')
+    def test_should_open_proc_stat(self, publish_mock, open_mock):
+        open_mock.return_value = StringIO('')
+        self.collector.collect()
+        open_mock.assert_called_once_with('/proc/stat', 'r')
+
+    @patch.object(Collector, 'publish')
+    def test_should_work_with_synthetic_data(self, publish_mock):
+        with patch('__builtin__.open', Mock(return_value=StringIO(
+            'softirq 0 0 0 0 0 0 0 0 0 0 0'
+        ))):
+            self.collector.collect()
+
+        self.assertPublishedMany(publish_mock, {})
+
+        with patch('__builtin__.open', Mock(return_value=StringIO(
+            'softirq 55 1 2 3 4 5 6 7 8 9 10'
+        ))):
+            self.collector.collect()
+
+        self.assertPublishedMany(publish_mock, {
+            'total': 55.0,
+            '0': 1,
+            '1': 2,
+            '2': 3,
+            '3': 4,
+            '4': 5,
+            '5': 6,
+            '6': 7,
+            '7': 8,
+            '8': 9,
+            '9': 10,
+        })
+
+    @patch.object(Collector, 'publish')
+    def test_should_work_with_real_data(self, publish_mock):
+        SoftInterruptCollector.PROC = self.getFixturePath('proc_stat_1')
+        self.collector.collect()
+
+        self.assertPublishedMany(publish_mock, {})
+
+        SoftInterruptCollector.PROC = self.getFixturePath('proc_stat_2')
+        self.collector.collect()
+
+        metrics = {
+            'total': 4971, 
+            '0': 0, 
+            '1': 1729, 
+            '2': 2, 
+            '3': 240, 
+            '4': 31, 
+            '5': 0, 
+            '6': 0, 
+            '7': 1480, 
+            '8': 0, 
+            '9': 1489, 
+        }
+
+        self.setDocExample(collector=self.collector.__class__.__name__,
+                           metrics=metrics,
+                           defaultpath=self.collector.config['path'])
+        self.assertPublishedMany(publish_mock, metrics)
+
+################################################################################
+if __name__ == "__main__":
+    unittest.main()
