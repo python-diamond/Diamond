f7a5df2c03fdcd0afbc26273b1ad2ea6682e77e4
Tomas Neme <lacrymology@gmail.com>
2012-10-29 16:40:27 -0300

    module-level docstring

--- src/collectors/processmemory/processmemory.py
+++ src/collectors/processmemory/processmemory.py
@@ -1,1 +1,18 @@
 # coding=utf-8
+
+"""
+A Diamond collector that collects memory usage of each process defined in it's
+config file by matching them with their executable filepath or the process name.
+
+Example config file ProcessMemoryCollector.conf
+
+```
+enabled=True
+[process]
+[[postgres]]
+exe=^\/usr\/lib\/postgresql\/+d.+d\/bin\/postgres$
+name=^postgres,^pg
+```
+
+exe and name are both lists of comma-separated regexps.
+"""
