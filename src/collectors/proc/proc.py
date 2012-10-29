de024cd9274eceaf2cac63f2f8b740b2cc24b8eb
Rob Smith <kormoc@gmail.com>
2012-10-28 21:47:31 -0700

    Add in ProcessStatCollector

---
+++ src/collectors/proc/proc.py
@@ -0,0 +1,76 @@
+# coding=utf-8
+
+"""
+The ProcessStatCollector collects metrics on process stats from
+/proc/stat
+
+#### Dependencies
+
+ * /proc/stat
+
+"""
+
+import platform
+import os
+import diamond.collector
+
+# Detect the architecture of the system
+# and set the counters for MAX_VALUES
+# appropriately. Otherwise, rolling over
+# counters will cause incorrect or
+# negative values.
+if platform.architecture()[0] == '64bit':
+    counter = (2 ** 64) - 1
+else:
+    counter = (2 ** 32) - 1
+
+
+class ProcessStatCollector(diamond.collector.Collector):
+
+    PROC = '/proc/stat'
+
+    def get_default_config_help(self):
+        config_help = super(ProcessStatCollector, self).get_default_config_help()
+        config_help.update({
+        })
+        return config_help
+
+    def get_default_config(self):
+        """
+        Returns the default collector settings
+        """
+        config = super(ProcessStatCollector, self).get_default_config()
+        config.update({
+            'path':     'softirq'
+        })
+        return config
+
+    def collect(self):
+        """
+        Collect process stat data
+        """
+        if not os.access(self.PROC, os.R_OK):
+            return False
+
+        #Open PROC file
+        file = open(self.PROC, 'r')
+        
+        #Get data
+        for line in file:
+            
+            if line.startswith('ctxt') or line.startswith('processes'):
+                data = line.split()
+                metric_name = data[0]
+                metric_value = int(data[1])
+                metric_value = int(self.derivative(metric_name,
+                                        long(metric_value), counter))
+                self.publish(metric_name, metric_value)
+
+            if line.startswith('procs_') or line.startswith('btime'):
+                data = line.split()
+                metric_name = data[0]
+                metric_value = int(data[1])
+                self.publish(metric_name, metric_value)
+
+        #Close file
+        file.close()
---
+++ src/collectors/proc/test/testproc.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+# coding=utf-8
+################################################################################
+
+from test import CollectorTestCase
+from test import get_collector_config
+from test import unittest
+from mock import Mock
+from mock import patch
+
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from StringIO import StringIO
+
+from diamond.collector import Collector
+from proc import ProcessStatCollector
+
+################################################################################
+
+
+class TestProcessStatCollector(CollectorTestCase):
+    def setUp(self):
+        config = get_collector_config('ProcessStatCollector', {
+            'interval': 1
+        })
+
+        self.collector = ProcessStatCollector(config, None)
+
+    @patch('__builtin__.open')
+    @patch('os.access', Mock(return_value=True))
+    @patch.object(Collector, 'publish')
+    def test_should_open_proc_stat(self, publish_mock, open_mock):
+        open_mock.return_value = StringIO('')
+        self.collector.collect()
+        open_mock.assert_called_once_with('/proc/stat', 'r')
+
+    @patch.object(Collector, 'publish')
+    def test_should_work_with_real_data(self, publish_mock):
+        ProcessStatCollector.PROC = self.getFixturePath('proc_stat_1')
+        self.collector.collect()
+
+        self.assertPublishedMany(publish_mock, {})
+
+        ProcessStatCollector.PROC = self.getFixturePath('proc_stat_2')
+        self.collector.collect()
+
+        metrics = {
+            'ctxt': 0,
+            'btime': 1319181102,
+            'processes': 0,
+            'procs_running': 1,
+            'procs_blocked': 0,
+            'ctxt': 1791,
+            'btime': 1319181102,
+            'processes': 2,
+            'procs_running': 1,
+            'procs_blocked': 0,
+        }
+
+        self.setDocExample(collector=self.collector.__class__.__name__,
+                           metrics=metrics,
+                           defaultpath=self.collector.config['path'])
+        self.assertPublishedMany(publish_mock, metrics)
+
+################################################################################
+if __name__ == "__main__":
+    unittest.main()
