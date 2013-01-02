863c7f6c418f46460088dfbe7310b0834ab7bb8b
Rob Smith <kormoc@gmail.com>
2013-01-01 18:12:05 -0800

    Rework the udp collector to be the same as the tcp collector, also add in unit testing

--- src/collectors/udp/udp.py
+++ src/collectors/udp/udp.py
@@ -15,12 +15,19 @@ import os
 
 class UDPCollector(diamond.collector.Collector):
 
-    PROC = '/proc/net/snmp'
+    PROC = [
+        '/proc/net/snmp'
+    ]
+
+    def __init__(self, config, handlers):
+        super(UDPCollector, self).__init__(config, handlers)
+        if self.config['allowed_names'] is None:
+            self.config['allowed_names'] = []
 
     def get_default_config_help(self):
         config_help = super(UDPCollector, self).get_default_config_help()
         config_help.update({
-            'allowed_names': 'list of entries to collect',
+            'allowed_names': 'list of entries to collect, empty to collect all',
         })
         return config_help
 
@@ -37,27 +44,55 @@ class UDPCollector(diamond.collector.Collector):
         return config
 
     def collect(self):
-        if not os.access(self.PROC, os.R_OK):
-            return None
+        metrics = {}
 
-        lines = []
+        for filepath in self.PROC:
+            if not os.access(filepath, os.R_OK):
+                self.log.error('Permission to access %s denied', filepath)
+                continue
 
-        file = open(self.PROC)
-        for line in file:
-            if line.startswith("Udp:"):
-                lines.append(line[5:].split())
-        file.close()
+            header = ''
+            data = ''
 
-        if len(lines) != 2:
-            return
+            # Seek the file for the lines that start with Tcp
+            file = open(filepath)
 
-        # There are two lines in lines: names and values, space-separated.
-        names, values = lines
-        allowed_names = self.config['allowed_names']
+            if not file:
+                self.log.error('Failed to open %s', filepath)
+                continue
 
-        for key, value in zip(names, values):
-            if key in allowed_names:
-                value = self.derivative(key, long(value))
-                if value < 0:
-                    continue
-                self.publish(key, value, 0)
+            while True:
+                line = file.readline()
+
+                # Reached EOF?
+                if len(line) == 0:
+                    break
+
+                # Line has metrics?
+                if line.startswith("Udp"):
+                    header = line
+                    data = file.readline()
+                    break
+            file.close()
+
+            # No data from the file?
+            if header == '' or data == '':
+                self.log.error('%s has no lines with Udp', filepath)
+                continue
+
+            header = header.split()
+            data = data.split()
+
+            for i in xrange(1, len(header)):
+                metrics[header[i]] = data[i]
+
+        for metric_name in metrics.keys():
+            if (len(self.config['allowed_names']) > 0
+                and metric_name not in self.config['allowed_names']):
+                continue
+
+            value = metrics[metric_name]
+            value = self.derivative(metric_name, long(value))
+
+            # Publish the metric
+            self.publish(metric_name, value, 0)
