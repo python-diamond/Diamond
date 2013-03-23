55eee9b33141838be9e74493397e8d7f722bc843
Mike Clarke <mike@disqus.com>
2013-03-22 18:20:27 -0700

    Remove conflicts for compatibility with older postgres installs

--- src/collectors/postgres/postgres.py
+++ src/collectors/postgres/postgres.py
@@ -195,7 +195,6 @@ class DatabaseStats(QueryStats):
                pg_stat_database.tup_inserted as tup_inserted,
                pg_stat_database.tup_updated as tup_updated,
                pg_stat_database.tup_deleted as tup_deleted,
-               pg_stat_database.conflicts as conflicts,
                pg_database_size(pg_database.datname) AS size
         FROM pg_database
         JOIN pg_stat_database
