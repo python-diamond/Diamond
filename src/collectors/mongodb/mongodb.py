aec83bb53ad7b9bde4b6e78643d3e4f51d215e49
Anton Tolchanov <me@knyar.net>
2013-04-04 20:43:08 +0000

    Add multiple instance support to mongodb collector
    Users can now define a list of hosts and collector will be fetching
    metrics from all of them. This is useful when you run several mongo
    instances on the same host (shard server and configuration server, for
    example).
    
    The change is backwards-compatible.

--- src/collectors/mongodb/mongodb.py
+++ src/collectors/mongodb/mongodb.py
@@ -31,7 +31,9 @@ class MongoDBCollector(diamond.collector.Collector):
     def get_default_config_help(self):
         config_help = super(MongoDBCollector, self).get_default_config_help()
         config_help.update({
-            'host': 'Hostname',
+            'hosts': 'Array of hostname(:port) elements to get metrics from',
+            'host': 'A single hostname(:port) to get metrics from'
+                   ' (can be used instead of hosts and overrides it)',
             'databases': 'A regex of which databases to gather metrics for.'
                         ' Defaults to all databases.',
             'ignore_collections': 'A regex of which collections to ignore.'
@@ -48,7 +50,7 @@ class MongoDBCollector(diamond.collector.Collector):
         config = super(MongoDBCollector, self).get_default_config()
         config.update({
             'path':      'mongo',
-            'host':      'localhost',
+            'hosts':     ['localhost'],
             'databases': '.*',
             'ignore_collections': '^tmp\.mr\.',
         })
@@ -61,34 +63,46 @@ class MongoDBCollector(diamond.collector.Collector):
             self.log.error('Unable to import pymongo')
             return {}
 
-        try:
-            if ReadPreference is None:
-                conn = pymongo.Connection(self.config['host'])
-            else:
-                conn = pymongo.Connection(
-                    self.config['host'],
-                    read_preference=ReadPreference.SECONDARY)
-        except Exception, e:
-            self.log.error('Couldnt connect to mongodb: %s', e)
-            return {}
-        data = conn.db.command('serverStatus')
-        self._publish_dict_with_prefix(data, [])
+        # we need this for backwards compatibility
+        if 'host' in self.config:
+            self.config['hosts'] = [self.config['host']]
 
-        db_name_filter = re.compile(self.config['databases'])
-        ignored_collections = re.compile(self.config['ignore_collections'])
-        for db_name in conn.database_names():
-            if not db_name_filter.search(db_name):
-                continue
-            db_stats = conn[db_name].command('dbStats')
-            self._publish_dict_with_prefix(db_stats, ['databases', db_name])
-            for collection_name in conn[db_name].collection_names():
-                if ignored_collections.search(collection_name):
+        for host in self.config['hosts']:
+            if len(self.config['hosts']) == 1:
+                # one host only, no need to have a prefix
+                base_prefix = []
+            else:
+                base_prefix = [re.sub('[:\.]', '_', host)]
+
+            try:
+                if ReadPreference is None:
+                    conn = pymongo.Connection(host)
+                else:
+                    conn = pymongo.Connection(
+                        host,
+                        read_preference=ReadPreference.SECONDARY)
+            except Exception, e:
+                self.log.error('Couldnt connect to mongodb: %s', e)
+                return {}
+            data = conn.db.command('serverStatus')
+            self._publish_dict_with_prefix(data, base_prefix)
+
+            db_name_filter = re.compile(self.config['databases'])
+            ignored_collections = re.compile(self.config['ignore_collections'])
+            for db_name in conn.database_names():
+                if not db_name_filter.search(db_name):
                     continue
-                collection_stats = conn[db_name].command('collstats',
-                                                         collection_name)
-                self._publish_dict_with_prefix(collection_stats,
-                                               ['databases', db_name,
-                                                collection_name])
+                db_stats = conn[db_name].command('dbStats')
+                db_prefix = base_prefix + ['databases', db_name]
+                self._publish_dict_with_prefix(db_stats, db_prefix)
+                for collection_name in conn[db_name].collection_names():
+                    if ignored_collections.search(collection_name):
+                        continue
+                    collection_stats = conn[db_name].command('collstats',
+                                                             collection_name)
+                    collection_prefix = db_prefix + [collection_name]
+                    self._publish_dict_with_prefix(collection_stats,
+                                                   collection_prefix)
 
     def _publish_dict_with_prefix(self, dict, prefix):
         for key in dict:
