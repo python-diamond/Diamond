5dc81cbb31ac4e5819b486760d25f42092b40d76
Andrei Vereha <avereha@gmail.com>
2013-05-03 11:22:12 +0300

    - compute percents and publish as counter some values in the mongodb collector

--- src/collectors/mongodb/mongodb.py
+++ src/collectors/mongodb/mongodb.py
@@ -28,17 +28,21 @@ except ImportError:
 
 class MongoDBCollector(diamond.collector.Collector):
 
+    def __init__(self, *args, **kwargs):
+        self.__totals = {}
+        super(MongoDBCollector, self).__init__(*args, **kwargs)
+
     def get_default_config_help(self):
         config_help = super(MongoDBCollector, self).get_default_config_help()
         config_help.update({
             'hosts': 'Array of hostname(:port) elements to get metrics from',
             'host': 'A single hostname(:port) to get metrics from'
-                   ' (can be used instead of hosts and overrides it)',
+                    ' (can be used instead of hosts and overrides it)',
             'databases': 'A regex of which databases to gather metrics for.'
-                        ' Defaults to all databases.',
+                         ' Defaults to all databases.',
             'ignore_collections': 'A regex of which collections to ignore.'
-                                 ' MapReduce temporary collections (tmp.mr.*)'
-                                 ' are ignored by default.',
+                                  ' MapReduce temporary collections (tmp.mr.*)'
+                                  ' are ignored by default.',
 
         })
         return config_help
@@ -86,7 +90,7 @@ class MongoDBCollector(diamond.collector.Collector):
                 return {}
             data = conn.db.command('serverStatus')
             self._publish_dict_with_prefix(data, base_prefix)
-
+            self._publish_transformed(data, base_prefix)
             db_name_filter = re.compile(self.config['databases'])
             ignored_collections = re.compile(self.config['ignore_collections'])
             for db_name in conn.database_names():
@@ -104,18 +108,101 @@ class MongoDBCollector(diamond.collector.Collector):
                     self._publish_dict_with_prefix(collection_stats,
                                                    collection_prefix)
 
-    def _publish_dict_with_prefix(self, dict, prefix):
-        for key in dict:
-            self._publish_metrics(prefix, key, dict)
+    def _publish_transformed(self, data, base_prefix):
+        """ Publish values of type: counter or percent """
+        self._publish_dict_with_prefix(data.get('opcounters', {}),
+                                       base_prefix + ['opcounters_per_sec'],
+                                       self.publish_counter)
+        self._publish_dict_with_prefix(data.get('opcountersRepl', {}),
+                                       base_prefix + ['opcountersRepl_per_sec'],
+                                       self.publish_counter)
+        self._publish_metrics(base_prefix + ['backgroundFlushing_per_sec'],
+                              'flushes',
+                              data.get('backgroundFlushing', {}),
+                              self.publish_counter)
+        self._publish_dict_with_prefix(data.get('network', {}),
+                                       base_prefix + ['network_per_sec'],
+                                       self.publish_counter)
+        self._publish_metrics(base_prefix + ['extra_info_per_sec'],
+                              'page_faults',
+                              data.get('extra_info', {}),
+                              self.publish_counter)
 
-    def _publish_metrics(self, prev_keys, key, data):
+        def get_dotted_value(data, key_name):
+            key_name = key_name.split('.')
+            for i in key_name:
+                data = data.get(i, {})
+                if not data:
+                    return 0
+            return data
+
+        def compute_interval(data, total_name):
+            current_total = get_dotted_value(data, total_name)
+            total_key = '.'.join(base_prefix) + '.' + total_name
+            last_total = self.__totals.get(total_key, current_total)
+            interval = current_total - last_total
+            self.__totals[total_key] = current_total
+            return interval
+
+        def publish_percent(value_name, total_name, data):
+            value = float(get_dotted_value(data, value_name) * 100)
+            interval = compute_interval(data, total_name)
+            key = '.'.join(base_prefix) + '.percent.' + value_name
+            self.publish_counter(key, value, time_delta=bool(interval),
+                                 interval=interval)
+
+        publish_percent('globalLock.lockTime', 'globalLock.totalTime', data)
+        publish_percent('indexCounters.btree.misses',
+                        'indexCounters.btree.accesses', data)
+
+        locks = data.get('locks')
+        if locks:
+            if '.' in locks:
+                locks['_global_'] = locks['.']
+                del (locks['.'])
+            key_prefix = '.'.join(base_prefix) + '.percent.'
+            db_name_filter = re.compile(self.config['databases'])
+            interval = compute_interval(data, 'uptimeMillis')
+            for db_name in locks:
+                if not db_name_filter.search(db_name):
+                    continue
+                r = get_dotted_value(
+                    locks,
+                    '%s.timeLockedMicros.r' % db_name)
+                R = get_dotted_value(
+                    locks,
+                    '.%s.timeLockedMicros.R' % db_name)
+                value = float(r + R) / 10
+                if value:
+                    self.publish_counter(key_prefix + 'locks.%s.read' % db_name,
+                                         value, time_delta=bool(interval),
+                                         interval=interval)
+                w = get_dotted_value(
+                    locks,
+                    '%s.timeLockedMicros.w' % db_name)
+                W = get_dotted_value(
+                    locks,
+                    '%s.timeLockedMicros.W' % db_name)
+                value = float(w + W) / 10
+                if value:
+                    self.publish_counter(
+                        key_prefix + 'locks.%s.write' % db_name,
+                        value, time_delta=bool(interval), interval=interval)
+
+    def _publish_dict_with_prefix(self, dict, prefix, publishfn=None):
+        for key in dict:
+            self._publish_metrics(prefix, key, dict, publishfn)
+
+    def _publish_metrics(self, prev_keys, key, data, publishfn=None):
         """Recursively publish keys"""
         value = data[key]
         keys = prev_keys + [key]
+        if not publishfn:
+            publishfn = self.publish
         if isinstance(value, dict):
             for new_key in value:
                 self._publish_metrics(keys, new_key, value)
         elif isinstance(value, int) or isinstance(value, float):
-            self.publish('.'.join(keys), value)
+            publishfn('.'.join(keys), value)
         elif isinstance(value, long):
-            self.publish('.'.join(keys), float(value))
+            publishfn('.'.join(keys), float(value))
