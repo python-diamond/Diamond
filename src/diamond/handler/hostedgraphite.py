be5a88b45f23d277d1fca69e237ae6e8325e8c1b
Rob Smith <kormoc@gmail.com>
2013-06-12 15:17:59 -0700

    Add in a hosted graphite handler

---
+++ src/diamond/handler/hostedgraphite.py
@@ -0,0 +1,38 @@
+# coding=utf-8
+
+"""
+[Hosted Graphite](https://www.hostedgraphite.com/) is the powerful open-source
+application metrics system used by hundreds of companies. We take away the
+headaches of scaling, maintenance, and upgrades and let you do what you do
+best - write great software.
+
+#### Configuration
+
+Enable this handler
+
+ * handlers = diamond.handler.hostedgraphite.HostedGraphiteHandler,
+
+ * apikey = API_KEY
+
+"""
+
+from graphite import GraphiteHandler
+
+class HostedGraphiteHandler(Handler):
+
+    def __init__(self, config=None):
+        """
+        Create a new instance of the HostedGraphiteHandler class
+        """
+        self.key = self.config.get('apikey', '').lower().strip()
+        
+        config['host'] = 'carbon.hostedgraphite.com'
+
+        self.graphite = GraphiteHandler(config)
+
+    def process(self, metric):
+        """
+        Process a metric by sending it to graphite
+        """
+        metric = self.key + '.' + str(metric)
+        self.graphite.process(metric)
