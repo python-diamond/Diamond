e7442994cb2576f965b1cb3812bae85b8d837b80
John Watson <john@disqus.com>
2013-08-28 17:02:46 -0700

    Ignore None stats from postgres

--- src/collectors/postgres/postgres.py
+++ src/collectors/postgres/postgres.py
@@ -70,7 +70,7 @@ class PostgresqlCollector(diamond.collector.Collector):
         for klass in metrics:
             stat = klass(self.connections, underscore=self.config['underscore'])
             stat.fetch()
-            [self.publish(metric, value) for metric, value in stat]
+            [self.publish(metric, value) for metric, value in stat if value is not None]
 
         # Cleanup
         [conn.close() for conn in self.connections.itervalues()]
