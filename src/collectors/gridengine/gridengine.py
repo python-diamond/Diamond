b1f0a80c340b07171b1dc259c108c0a76fa07aca
Rob Smith <kormoc@gmail.com>
2013-10-08 19:55:28 -0700

    Disable gridengine by default, also update docs

--- src/collectors/gridengine/gridengine.py
+++ src/collectors/gridengine/gridengine.py
@@ -94,7 +94,6 @@ class GridEngineCollector(diamond.collector.Collector):
         config = super(GridEngineCollector, self).get_default_config()
         config.update({
             'bin_path': '/opt/gridengine/bin/lx-amd64/qstat',
-            'enabled': 'False',
             'method': 'Threaded',
             'path': 'gridengine',
             'sge_root': self._sge_root(),
