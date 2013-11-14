fa6601f9aac2b843b6dc13da65903c4ebf38f85b
Farid Saad <faradio@gmail.com>
2013-11-14 15:51:06 -0600

    Adding DRBD collector

---
+++ src/collectors/drbd/test/testdrbd.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# coding=utf-8
+################################################################################
+
+from test import CollectorTestCase
+from test import get_collector_config
+from test import unittest
+from mock import patch
+
+from diamond.collector import Collector
+from drbd import DRBDCollector
+
+################################################################################
+
+
+class TestDRBDCollector(CollectorTestCase):
+    def setUp(self):
+        config = get_collector_config('DRBDCollector', {})
+
+        self.collector = DRBDCollector(config, None)
+
+    def test_import(self):
+        self.assertTrue(DRBDCollector)
+
+################################################################################
+if __name__ == "__main__":
+    unittest.main()
