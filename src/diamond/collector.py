4967ffc963b99043abd092852526cc7ced4951d7
Rob Smith <kormoc@gmail.com>
2013-12-21 14:13:35 -0800

    Fixes #491, this moves to the unified str_to_bool and extends it with extra boolish strings

--- src/diamond/collector.py
+++ src/diamond/collector.py
@@ -109,13 +109,17 @@ get_hostname.cached_results = {}
 
 def str_to_bool(value):
     """
-    Converts string ('true', 'false') to bool
+    Converts string truthy/falsey strings to a bool
+    Empty strings are false
     """
     if isinstance(value, basestring):
-        if value.strip().lower() == 'true':
+        value = value.strip().lower()
+        if value in ['true', 't', 'yes', 'y']:
             return True
-        else:
+        elif value in ['false', 'f', 'no', 'n', '']:
             return False
+        else:
+            raise NotImplementedError("Unknown bool %s" % value)
 
     return value
 
