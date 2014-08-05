af0b955fbb0f619096ce3f8adcf36d4a2a6d4c53
Renzo Toma <rtoma@bol.com>
2014-08-05 13:37:12 +0200

    fix(feat): better exception handling in uptime collector to pass tests
    Change-Id: I6a63233247224bb674cf4866dbc3cf2183a3cb75

--- src/collectors/uptime/uptime.py
+++ src/collectors/uptime/uptime.py
@@ -7,16 +7,6 @@ procpath = '/proc/uptime'
 metric_name = 'minutes'
 
 
-def read():
-    try:
-        fd = open(procpath)
-        uptime = fd.readline()
-    finally:
-        fd.close()
-    v = float(uptime.split()[0].strip())
-    return convertor.time.convert(v, 's', 'm')
-
-
 class UptimeCollector(Collector):
     def get_default_config(self):
         config = super(UptimeCollector, self).get_default_config()
@@ -27,12 +17,20 @@ class UptimeCollector(Collector):
 
     def collect(self):
         if not os.path.exists(procpath):
-            self.log.error('path: {0} not found'.format(procpath))
+            self.log.error('Input path %s does not exist' % procpath)
             return {}
 
-        try:
-            v = read()
-        except Exception as e:
-            self.log.exception(e)
+        v = self.read()
+        if not v is None:
+            self.publish(metric_name, v)
 
-        self.publish(metric_name, v)
+    def read(self):
+        try:
+            fd = open(procpath)
+            uptime = fd.readline()
+            fd.close()
+            v = float(uptime.split()[0].strip())
+            return convertor.time.convert(v, 's', 'm')
+        except Exception, e:
+            self.log.error('Unable to read uptime from %s: %s' % (procpath, e))
+            return None
