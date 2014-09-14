b2fa87343e5929ab3280d7792fd03208199d950e
Rob Smith <kormoc@gmail.com>
2014-09-14 13:43:19 -0700

    Fixes #612, this allows the collector to search mutliple paths for connection tracking metrics

--- src/collectors/conntrack/conntrack.py
+++ src/collectors/conntrack/conntrack.py
@@ -11,6 +11,7 @@ net.netfilter.nf_conntrack_count_max
 """
 
 import diamond.collector
+import os
 
 
 class ConnTrackCollector(diamond.collector.Collector):
@@ -24,8 +25,8 @@ class ConnTrackCollector(diamond.collector.Collector):
         """
         config_help = super(ConnTrackCollector, self).get_default_config_help()
         config_help.update({
-            "dir":         "Directory with files of interest",
-            "files":       "List of files to collect statistics from",
+            "dir":      "Directories with files of interest, comma seperated",
+            "files":    "List of files to collect statistics from",
         })
         return config_help
 
@@ -35,9 +36,10 @@ class ConnTrackCollector(diamond.collector.Collector):
         """
         config = super(ConnTrackCollector, self).get_default_config()
         config.update({
-            "path":             "conntrack",
-            "dir":              "/proc/sys/net/ipv4/netfilter",
-            "files":            "ip_conntrack_count,ip_conntrack_max",
+            "path":  "conntrack",
+            "dir":   "/proc/sys/net/ipv4/netfilter,/proc/sys/net/netfilter",
+            "files": "ip_conntrack_count,ip_conntrack_max,"
+            "nf_conntrack_count,nf_conntrack_max",
         })
         return config
 
@@ -48,20 +50,36 @@ class ConnTrackCollector(diamond.collector.Collector):
         collected = {}
         files = []
 
+        if isinstance(self.config['dir'], basestring):
+            dirs = [d.strip() for d in self.config['dir'].split(',')]
+        elif isinstance(self.config['dir'], list):
+            dirs = self.config['dir']
+
         if isinstance(self.config['files'], basestring):
             files = [f.strip() for f in self.config['files'].split(',')]
         elif isinstance(self.config['files'], list):
             files = self.config['files']
 
-        for sfile in files:
-            fpath = "%s/%s" % (self.config["dir"], sfile)
-            try:
-                with open(fpath, "r") as fhandle:
-                    collected[sfile] = float(fhandle.readline().rstrip("\n"))
-            except Exception as exception:
-                self.log.error("Failed to collect from '%s': %s",
-                               fpath,
-                               exception)
+        for sdir in dirs:
+            for sfile in files:
+                fpath = "%s/%s" % (sdir, sfile)
+                if sfile[-15:] == 'conntrack_count':
+                    metric_name = 'ip_conntrack_count'
+                elif sfile[-13:] == 'conntrack_max':
+                    metric_name = 'ip_conntrack_max'
+                else:
+                    self.log.error('Unknown file for collection: %s', sfile)
+                    continue
+                if not os.path.exists(fpath):
+                    continue
+                try:
+                    with open(fpath, "r") as fhandle:
+                        metric = float(fhandle.readline().rstrip("\n"))
+                        collected[metric_name] = metric
+                except Exception as exception:
+                    self.log.error("Failed to collect from '%s': %s",
+                                   fpath,
+                                   exception)
 
         for key in collected.keys():
             self.publish(key, collected[key])
