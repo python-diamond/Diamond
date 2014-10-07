b7064ef388d0b076ec6584d1fb0f24244218fedf
Rob Smith <kormoc@gmail.com>
2014-10-06 21:12:37 -0700

    Simplify the collector code. Also allow catching USR1 and USR2 signals

--- src/diamond/utils/scheduler.py
+++ src/diamond/utils/scheduler.py
@@ -10,16 +10,10 @@ try:
 except ImportError:
     setproctitle = None
 
-
-class CollectorTimeoutException(Exception):
-    pass
-
-
-def collector_timeout(signum, frame):
-    """
-    Called by the timeout alarm during the collector run time
-    """
-    raise CollectorTimeoutException()
+from diamond.utils.signals import signal_to_exception
+from diamond.utils.signals import SIGALRMException
+from diamond.utils.signals import SIGUSR1Exception
+from diamond.utils.signals import SIGUSR2Exception
 
 
 def collector_process(collector, metric_queue, log):
@@ -29,57 +23,55 @@ def collector_process(collector, metric_queue, log):
     if setproctitle:
         setproctitle('%s - %s' % (getproctitle(), proc.name))
 
-    signal.signal(signal.SIGALRM, collector_timeout)
+    signal.signal(signal.SIGALRM, signal_to_exception)
+    signal.signal(signal.SIGUSR1, signal_to_exception)
+    signal.signal(signal.SIGUSR2, signal_to_exception)
 
     interval = float(collector.config['interval'])
     max_time = int(interval * 0.9)
 
-    log.debug(
-        'Starting %s. Interval: %s seconds. Max collection time: %s seconds.',
-        proc.name,
-        interval,
-        max_time
-        )
+    log.debug('Starting')
+    log.debug('Interval: %s seconds', interval)
+    log.debug('Max collection time: %s seconds', max_time)
 
     # Validate the interval
     if interval <= 0:
         log.critical('interval of %s is not valid!', interval)
         sys.exit(1)
 
-    # When should we wake up again?
-    next_collection = time.time() + interval
+    next_collection = time.time()
 
     while(True):
         try:
-            try:
-                # Ensure collector run times fit into the collection window
-                signal.alarm(max_time)
-
-                # Collect!
-                collector._run()
-
-                # Success! Disable the alarm
-                signal.alarm(0)
-
-            except CollectorTimeoutException, e:
-                log.critical('Took too long to run! Killed!')
-
-            # TODO: Catch other signals and log correctly
-
-            # Any exception? Kill the thread
-            except Exception, e:
-                log.error(e)
-                break
-        finally:
-            # If the collector took too long to run, skip ahead to the next
-            # valid collection interval
-
-            while next_collection < time.time():
-                next_collection += interval
-
-            # Sleep until next collection time
-
             time.sleep(next_collection - time.time())
+
+            next_collection += interval
+
+            # Ensure collector run times fit into the collection window
+            signal.alarm(max_time)
+
+            # Collect!
+            collector._run()
+
+            # Success! Disable the alarm
+            signal.alarm(0)
+
+        except SIGALRMException, e:
+            log.error('Took too long to run! Killed!')
+            continue
+
+        except SIGUSR1Exception:
+            log.debug('Received USR1')
+            pass
+
+        except SIGUSR2Exception:
+            log.debug('Received USR2')
+            pass
+
+        # Any other exception? Kill the thread
+        except Exception, e:
+            log.error('%s(%s)', e, e.args)
+            break
 
 
 def handler_process(handlers, metric_queue, log):
---
+++ src/diamond/utils/signals.py
@@ -0,0 +1,34 @@
+# coding=utf-8
+
+import signal
+
+def signal_to_exception(signum, frame):
+    """
+    Called by the timeout alarm during the collector run time
+    """
+    if signum == signal.SIGALRM:
+        raise SIGALRMException()
+    if signum == signal.SIGTERM:
+        raise SIGTERMException()
+    if signum == signal.SIGUSR1:
+        raise SIGUSR1Exception()
+    if signum == signal.SIGUSR2:
+        raise SIGUSR2Exception()
+    raise SignalException(signum)
+
+
+class SignalException(Exception):
+    pass
+
+
+class SIGALRMException(SignalException):
+    pass
+
+
+class SIGUSR1Exception(SignalException):
+    pass
+
+
+class SIGUSR2Exception(SignalException):
+    pass
+
