a332d8b79b14e7539a5fdc73477bb0aec7d1e6b4
Rob Smith <kormoc@gmail.com>
2014-10-06 22:52:54 -0700

    pep8/pyflakes

--- src/diamond/utils/signals.py
+++ src/diamond/utils/signals.py
@@ -2,14 +2,13 @@
 
 import signal
 
+
 def signal_to_exception(signum, frame):
     """
     Called by the timeout alarm during the collector run time
     """
     if signum == signal.SIGALRM:
         raise SIGALRMException()
-    if signum == signal.SIGTERM:
-        raise SIGTERMException()
     if signum == signal.SIGUSR1:
         raise SIGUSR1Exception()
     if signum == signal.SIGUSR2:
@@ -31,4 +30,3 @@ class SIGUSR1Exception(SignalException):
 
 class SIGUSR2Exception(SignalException):
     pass
-
