a2bde9d38bca99e5d71371ac22ea74ea6cae7d83
Mathieu Lecarme <mlecarme@bearstech.com>
2014-10-16 11:44:49 +0200

    Some modules use the warnings API.

--- src/diamond/collector.py
+++ src/diamond/collector.py
@@ -165,6 +165,7 @@ class Collector(object):
         Create a new instance of the Collector class
         """
         # Initialize Logger
+        logging.captureWarnings(True)
         self.log = logging.getLogger('diamond')
         # Initialize Members
         self.name = self.__class__.__name__
