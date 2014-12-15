python-diamond/Diamond

commit cb2cd7332bc21af299f81e275b4f46f478eb9f5a

Changes allow for a list of matches and include kafka stats by default.

src/collectors/kafka/kafka.py
@@ -8,7 +8,6 @@ Collect stats via MX4J from Kafka
  * urllib2
  * xml.etree
 """
-
 import urllib2
 
 from urllib import urlencode
@@ -29,19 +28,6 @@ import diamond.collector
 
 
 class KafkaCollector(diamond.collector.Collector):
-    MBEAN_WHITELIST = frozenset([
-        'kafka.log.LogStats',
-        'kafka.network.SocketServerStats',
-        'kafka.message.LogFlushStats',
-        'kafka.server.BrokerTopicStat',
-    ])
-
-    JVM_MBEANS = {
-        'java.lang:type=GarbageCollector,name=PS MarkSweep': 'jvm.gc.marksweep',
-        'java.lang:type=GarbageCollector,name=PS Scavenge': 'jvm.gc.scavenge',
-        'java.lang:type=Threading': 'jvm.threading',
-    }
-
     ATTRIBUTE_TYPES = {
         'double': float,
         'float': float,
@@ -95,8 +81,8 @@ class KafkaCollector(diamond.collector.Collector):
             self.log.error("Unable to parse response from mx4j")
             return None
 
-    def get_mbeans(self):
-        query_args = {'querynames': 'kafka:*'}
+    def get_mbeans(self, pattern):
+        query_args = {'querynames': pattern}
 
         mbeans = self._get('/serverbydomain', query_args)
         if mbeans is None:
@@ -106,14 +92,11 @@ class KafkaCollector(diamond.collector.Collector):
 
         for mbean in mbeans.getiterator(tag='MBean'):
             classname = mbean.get('classname')
-
-            if classname not in self.MBEAN_WHITELIST:
-                continue
-
             objectname = mbean.get('objectname')
+
             if objectname:
                 found_beans.add(objectname)
-
+        
         return found_beans
 
     def query_mbean(self, objectname, key_prefix=None):
@@ -129,7 +112,23 @@ class KafkaCollector(diamond.collector.Collector):
             return
 
         if key_prefix is None:
-            key_prefix = objectname.split('=')[1]
+            # Could be 1 or 2 = in the string
+            # java.lang:type=Threading
+            # "kafka.controller":type="ControllerStats",name="LeaderElectionRateAndTimeMs"
+            split_num = objectname.count('=')
+            for i in range(split_num):
+              if i == 0:
+                key_prefix = objectname.split('=')[1]
+                if '"' in key_prefix:
+                  key_prefix = key_prefix.split('"')[1]
+                if "," in key_prefix:
+                  key_prefix = key_prefix.split(',')[0]
+              elif i > 0:
+                key = objectname.split('=')[2]
+                if key:
+                  if '"' in key:
+                    key = key.split('"')[1]
+                  key_prefix = key_prefix + '.' + key
 
         metrics = {}
 
@@ -154,17 +153,17 @@ class KafkaCollector(diamond.collector.Collector):
             return
 
         # Get list of gatherable stats
-        mbeans = self.get_mbeans()
+        query_list = [ '*kafka*:*', 'java.lang:type=GarbageCollector,name=*', 'java.lang:type=Threading' ]
+        mbeans = set()
+        for pattern in query_list:
+          match = self.get_mbeans(pattern)
+          mbeans.update(match)
 
         metrics = {}
 
         # Query each one for stats
         for mbean in mbeans:
             stats = self.query_mbean(mbean)
-            metrics.update(stats)
-
-        for mbean, key_prefix in self.JVM_MBEANS.iteritems():
-            stats = self.query_mbean(mbean, key_prefix)
             metrics.update(stats)
 
         # Publish stats

commit 68a2fca44a0d7a78c700d09a3b71843457316369

Fix kafka unit tests
This was broken in cb2cd73. There is no longer a key prefix for JVM metrics.
Thus the metric names have changed and needed to be sanitized before reporting

src/collectors/kafka/kafka.py
@@ -143,6 +143,9 @@ class KafkaCollector(diamond.collector.Collector):
             value = ptype(attrib.get('value'))
 
             name = '.'.join([key_prefix, attrib.get('name')])
+            # Some prefixes and attributes could have spaces, thus we must
+            # sanitize them
+            name = name.replace(' ', '')
 
             metrics[name] = value
 

src/collectors/kafka/test/fixtures/serverbydomain_gc.xml
@@ -1,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<Server>
+  <Domain name="java.lang">
+    <MBean classname="sun.management.GarbageCollectorImpl" description="Information on the management interface of the MBean" objectname="java.lang:type=GarbageCollector,name=PS MarkSweep"/>
+    <MBean classname="sun.management.GarbageCollectorImpl" description="Information on the management interface of the MBean" objectname="java.lang:type=GarbageCollector,name=PS Scavenge"/>
+  </Domain>
+</Server>

src/collectors/kafka/test/fixtures/serverbydomain_threading.xml
@@ -1,0 +1,6 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<Server>
+  <Domain name="java.lang">
+    <MBean classname="sun.management.ThreadImpl" description="Information on the management interface of the MBean" objectname="java.lang:type=Threading"/>
+  </Domain>
+</Server>

src/collectors/kafka/test/testkafka.py
@@ -151,9 +151,11 @@ class TestKafkaCollector(CollectorTestCase):
     def test(self, publish_mock, urlopen_mock):
         urlopen_mock.side_effect = [
             self.getFixture('serverbydomain_logs_only.xml'),
-            self.getFixture('mbean.xml'),
-            self.getFixture('gc_marksweep.xml'),
+            self.getFixture('serverbydomain_gc.xml'),
+            self.getFixture('serverbydomain_threading.xml'),
             self.getFixture('gc_scavenge.xml'),
+            self.getFixture('gc_marksweep.xml'),
+            self.getFixture('mbean.xml'),
             self.getFixture('threading.xml'),
         ]
         self.collector.collect()
@@ -163,16 +165,16 @@ class TestKafkaCollector(CollectorTestCase):
             'kafka.logs.mytopic-1.NumAppendedMessages': 224634137,
             'kafka.logs.mytopic-1.NumberOfSegments': 94,
             'kafka.logs.mytopic-1.Size': 50143615339,
-            'jvm.threading.CurrentThreadCpuTime': 0,
-            'jvm.threading.CurrentThreadUserTime': 0,
-            'jvm.threading.DaemonThreadCount': 58,
-            'jvm.threading.PeakThreadCount': 90,
-            'jvm.threading.ThreadCount': 89,
-            'jvm.threading.TotalStartedThreadCount': 228,
-            'jvm.gc.scavenge.CollectionCount': 37577,
-            'jvm.gc.scavenge.CollectionTime': 112293,
-            'jvm.gc.marksweep.CollectionCount': 2,
-            'jvm.gc.marksweep.CollectionTime': 160,
+            'Threading.CurrentThreadCpuTime': 0,
+            'Threading.CurrentThreadUserTime': 0,
+            'Threading.DaemonThreadCount': 58,
+            'Threading.PeakThreadCount': 90,
+            'Threading.ThreadCount': 89,
+            'Threading.TotalStartedThreadCount': 228,
+            'GarbageCollector.PSScavenge.CollectionCount': 37577,
+            'GarbageCollector.PSScavenge.CollectionTime': 112293,
+            'GarbageCollector.PSMarkSweep.CollectionCount': 2,
+            'GarbageCollector.PSMarkSweep.CollectionTime': 160,
         }
 
         self.assertPublishedMany(publish_mock, expected_metrics)