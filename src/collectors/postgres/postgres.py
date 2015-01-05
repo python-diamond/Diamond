e75a9ab6f224dd1a32db6e93a60921e028c984af
Rob Smith <kormoc@gmail.com>
2015-01-04 19:21:54 -0800

    pep8 fixes

--- src/collectors/postgres/postgres.py
+++ src/collectors/postgres/postgres.py
@@ -95,7 +95,8 @@ class PostgresqlCollector(diamond.collector.Collector):
         # Iterate every QueryStats class
         for metric_name in set(metrics):
             if metric_name not in metrics_registry:
-                self.log.error('metric_name %s not found in metric registry' % metric_name)
+                self.log.error(
+                    'metric_name %s not found in metric registry' % metric_name)
                 continue
 
             for dbase in dbs:
