6c51f5b6e80860182c2692b271f10bae6be85a63
Shawn Butts <me@shawnbutts.com>
2015-01-29 17:30:43 -0500

    Add HttpJSONPostHandler
    Post JSON formatted metrics to an HTTP endpoint

---
+++ src/diamond/handler/httpjsonHandler.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""
+Send metrics in JSON to a http endpoint via POST
+
+# Dependencies
+
+ * urllib2
+
+
+# Configuration
+Enable this handler
+
+ * handers = diamond.handler.httpjsonHandler.HttpJSONPostHandler
+
+ * url = http://www.example.com/endpoint
+
+"""
+
+from Handler import Handler
+import logging
+import urllib2
+import json
+
+
+class HttpJSONPostHandler(Handler):
+
+    # Inititalize Handler with url and batch size
+    def __init__(self, config=None):
+        Handler.__init__(self, config)
+        self.metrics = []
+        self.batch_size = int(self.config['batch'])
+        self.url = self.config.get('url')
+
+    def get_default_config_help(self):
+        """
+        Returns the help text for the configuration options for this handler
+        """
+        config = super(HttpJSONPostHandler, self).get_default_config_help()
+
+        config.update({
+            'url': 'Fully qualified url to send metrics to',
+            'batch':
+            'How many to store before sending to the graphite server',
+            'headers':
+            'Header variable if needed. Will be added to every request',
+        })
+
+        return config
+
+    def get_default_config(self):
+        """
+        Return the default config for the handler
+        """
+        config = super(HttpJSONPostHandler, self).get_default_config()
+
+        config.update({
+            'url': 'http://httpbin.org/post',
+            'batch': 100,
+            'headers': {'User-Agent': 'Diamond HTTP JSON Handler'},
+        })
+
+        return config
+
+    # Join batched metrics and push to url mentioned in config
+    def process(self, metric):
+
+        self.metrics.append({
+            'name': metric.path,
+            'value': metric.value,
+            'raw_value': metric.raw_value,
+            'timestamp': metric.timestamp,
+            'metric_type': metric.metric_type,
+            'precision': metric.precision,
+            'host': metric.host,
+            'ttl': metric.ttl,
+            'path_prefix': metric.getPathPrefix(),
+            'collector_path': metric.getCollectorPath(),
+            'metric_path': metric.getMetricPath(),
+        })
+
+        if len(self.metrics) >= self.batch_size:
+            self.post()
+
+    # Overriding flush to post metrics for every collector.
+    def flush(self):
+        """Flush metrics in queue"""
+        self.post()
+
+    def post(self):
+        req = urllib2.Request(self.url,
+                              json.dumps((self.metrics)),
+                              headers=self.config['headers'])
+
+        if 'Content-type' not in self.config['headers']:
+            req.add_header('Content-type', 'application/json')
+
+        resp = urllib2.urlopen(req)
+        self.metrics = []
