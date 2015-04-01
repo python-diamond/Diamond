49ffcba6fab2b4a557060fb3700cfd72b33bfb3c
Russ Garrett <russ@garrett.co.uk>
2015-04-01 18:27:29 +0100

    Add IPv6 support to IPCollector
    The /proc/net/snmp file only contains IPv4 metrics. Additionally read
    IPv6 metrics from /proc/net/snmp6.

--- src/collectors/ip/ip.py
+++ src/collectors/ip/ip.py
@@ -33,9 +33,22 @@ class IPCollector(diamond.collector.Collector):
         '/proc/net/snmp',
     ]
 
+    PROC6 = [
+        '/proc/net/snmp6'
+    ]
+
     GAUGES = [
         'Forwarding',
         'DefaultTTL',
+    ]
+
+    DEFAULT_METRICS = [
+        'InAddrErrors', 'InDelivers', 'InDiscards', 'InHdrErrors',
+        'InReceives', 'InUnknownProtos', 'OutDiscards', 'OutNoRoutes',
+        'OutRequests',
+        'Ip6InAddrErrors', 'Ip6InDelivers', 'Ip6InDiscards', 'Ip6InHdrErrors',
+        'Ip6InReceives', 'Ip6InUnknownProtos', 'Ip6OutDiscards', 'Ip6OutNoRoutes',
+        'Ip6OutRequests'
     ]
 
     def process_config(self):
@@ -56,30 +69,38 @@ class IPCollector(diamond.collector.Collector):
         config = super(IPCollector, self).get_default_config()
         config.update({
             'path': 'ip',
-            'allowed_names': 'InAddrErrors, InDelivers, InDiscards, ' +
-            'InHdrErrors, InReceives, InUnknownProtos, OutDiscards, ' +
-            'OutNoRoutes, OutRequests'
+            'allowed_names': ','.join(self.DEFAULT_METRICS)
         })
         return config
 
+    def open_file(self, filepath):
+        if not os.access(filepath, os.R_OK):
+            self.log.error('Permission to access %s denied', filepath)
+            return
+
+        fp = open(filepath)
+
+        if not fp:
+            self.log.error('Failed to open %s', filepath)
+            return
+        return fp
+
     def collect(self):
+        self.collect_ipv4()
+        self.collect_ipv6()
+
+    def collect_ipv4(self):
         metrics = {}
 
         for filepath in self.PROC:
-            if not os.access(filepath, os.R_OK):
-                self.log.error('Permission to access %s denied', filepath)
+            file = self.open_file(filepath)
+            if not file:
                 continue
 
             header = ''
             data = ''
 
             # Seek the file for the lines which start with Ip
-            file = open(filepath)
-
-            if not file:
-                self.log.error('Failed to open %s', filepath)
-                continue
-
             while True:
                 line = file.readline()
 
@@ -118,3 +139,25 @@ class IPCollector(diamond.collector.Collector):
                 self.publish_gauge(metric_name, value, 0)
             else:
                 self.publish_counter(metric_name, value, 0)
+
+    def collect_ipv6(self):
+        metrics = {}
+        for filepath in self.PROC6:
+            fp = self.open_file(filepath)
+
+            for line in fp.readlines():
+                key, value = line.split()
+                metrics[key] = long(value)
+
+            fp.close()
+
+        for metric, value in metrics.iteritems():
+            if (len(self.config['allowed_names']) > 0
+                    and metric not in self.config['allowed_names']):
+                continue
+
+            # Publish the metric
+            if metric in self.GAUGES:
+                self.publish_gauge(metric, value, 0)
+            else:
+                self.publish_counter(metric, value, 0)
