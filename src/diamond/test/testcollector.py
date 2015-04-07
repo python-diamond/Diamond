4fae6537c8f5bd0b2f28af7afc1fa69f6d135427
Mateusz Moneta <mateuszm@opera.com>
2015-04-07 16:56:40 +0200

    [collector] Add tests for get_metric_path, handle case when path is empty.

--- src/diamond/test/testcollector.py
+++ src/diamond/test/testcollector.py
@@ -2,6 +2,7 @@
 # coding=utf-8
 ################################################################################
 
+from mock import patch
 from test import unittest
 import configobj
 
@@ -32,3 +33,78 @@ class BaseCollectorTest(unittest.TestCase):
         }
         c = Collector(config, [])
         self.assertEquals('custom.localhost', c.get_hostname())
+
+    @patch('diamond.collector.get_hostname')
+    def test_get_metric_path_no_prefix(self, get_hostname_mock):
+
+        config = configobj.ConfigObj()
+        config['collectors'] = {}
+        config['collectors']['default'] = {}
+        config['collectors']['default']['path_prefix'] = ''
+        config['collectors']['default']['path'] = 'bar'
+
+        get_hostname_mock.return_value = None
+
+        result = Collector(config, []).get_metric_path('foo')
+
+        self.assertEqual('bar.foo', result)
+
+    @patch('diamond.collector.get_hostname')
+    def test_get_metric_path_no_prefix_no_path(self, get_hostname_mock):
+
+        config = configobj.ConfigObj()
+        config['collectors'] = {}
+        config['collectors']['default'] = {}
+        config['collectors']['default']['path_prefix'] = ''
+        config['collectors']['default']['path'] = ''
+
+        get_hostname_mock.return_value = None
+
+        result = Collector(config, []).get_metric_path('foo')
+
+        self.assertEqual('foo', result)
+
+    @patch('diamond.collector.get_hostname')
+    def test_get_metric_path_no_path(self, get_hostname_mock):
+
+        config = configobj.ConfigObj()
+        config['collectors'] = {}
+        config['collectors']['default'] = {}
+        config['collectors']['default']['path_prefix'] = 'bar'
+        config['collectors']['default']['path'] = ''
+
+        get_hostname_mock.return_value = None
+
+        result = Collector(config, []).get_metric_path('foo')
+
+        self.assertEqual('bar.foo', result)
+
+    @patch('diamond.collector.get_hostname')
+    def test_get_metric_path_dot_path(self, get_hostname_mock):
+
+        config = configobj.ConfigObj()
+        config['collectors'] = {}
+        config['collectors']['default'] = {}
+        config['collectors']['default']['path_prefix'] = 'bar'
+        config['collectors']['default']['path'] = '.'
+
+        get_hostname_mock.return_value = None
+
+        result = Collector(config, []).get_metric_path('foo')
+
+        self.assertEqual('bar.foo', result)
+
+    @patch('diamond.collector.get_hostname')
+    def test_get_metric_path(self, get_hostname_mock):
+
+        config = configobj.ConfigObj()
+        config['collectors'] = {}
+        config['collectors']['default'] = {}
+        config['collectors']['default']['path_prefix'] = 'poof'
+        config['collectors']['default']['path'] = 'xyz'
+
+        get_hostname_mock.return_value = 'bar'
+
+        result = Collector(config, []).get_metric_path('foo')
+
+        self.assertEqual('poof.bar.xyz.foo', result)
