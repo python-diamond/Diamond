db2b4e1f1628d0ec748200de879956b89201bf39
Matthias Erll <matthias@precisdigital.com>
2015-06-06 19:40:59 +0200

    Added id truncating and updated docker cgroups.

--- src/collectors/memory_docker/memory_docker.py
+++ src/collectors/memory_docker/memory_docker.py
@@ -3,8 +3,14 @@
 """
 The MemoryDockerCollector collects memory statistics from docker containers.
 It collects identical information as ``MemoryCgroupCollector``. The only
-difference is that it replaces the container ids within the ``docker`` path
+difference is that it replaces container ids within ``docker`` paths
 with container names resolved through the Docker client.
+
+Docker metrics with ids that do not exist can be filtered out. Ids of
+containers that exist but have no name are always published.
+
+More recent versions of Docker create cgroups starting with ``docker-``.
+In that case the metrics are renamed to start with ``docker.``
 
 #### Dependencies
 
@@ -26,27 +32,51 @@ sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)),
 from memory_cgroup import MemoryCgroupCollector
 
 
-DOCKER_SUB_STR = 'docker.'
+DOCKER_SUB_STR = 'docker'
 DOCKER_SUB_LEN = len(DOCKER_SUB_STR)
+
+
+def _parse_metric_name(metric_name):
+    docker_idx = metric_name.find(DOCKER_SUB_STR)
+    if 0 <= docker_idx < len(metric_name):
+        start_idx = docker_idx + DOCKER_SUB_LEN + 1
+        sep = metric_name[start_idx - 1]
+        if sep in ('.', '-'):
+            dot_idx = metric_name.find('.', start_idx)
+            if dot_idx >= start_idx:
+                container_id = metric_name[start_idx:dot_idx]
+                if container_id != 'service':
+                    return container_id, sep, start_idx, dot_idx
+    return None
 
 
 class MemoryDockerCollector(MemoryCgroupCollector):
     def process_config(self):
         super(MemoryDockerCollector, self).process_config()
         self.filter_existing = str_to_bool(self.config['filter_existing'])
+        trunc = self.config['truncate_ids']
+        try:
+            self.truncate_ids = int(trunc)
+        except (TypeError, ValueError):
+            self.truncate_ids = 0
 
     def get_default_config_help(self):
         config_help = super(
             MemoryDockerCollector, self).get_default_config_help()
         config_help.update(
             filter_existing="If set to 'true', skips publishing information "
-                            "on cgroups without an existing container."
+                            "on cgroups without an existing container.",
+            truncate_ids="Length to truncate ids of non-existing containers "
+                         "and containers without a name. If set to equal to "
+                         "or lower than zero, metric names are not changed "
+                         "in length.",
         )
         return config_help
 
     def get_default_config(self):
         config = super(MemoryDockerCollector, self).get_default_config()
         config.update(filter_existing=False)
+        config.update(truncate_ids=0)
         return config
 
     def collect(self):
@@ -55,29 +85,39 @@ class MemoryDockerCollector(MemoryCgroupCollector):
             return
 
         self.containers = dict(
-            (c['Id'], c['Names'][0][1:])
+            (c['Id'], c['Names'][0][1:] if c['Names'] else '')
             for c in docker.Client().containers(all=True)
-            if c['Names'] is not None)
+        )
         return super(MemoryDockerCollector, self).collect()
 
     def publish(self, metric_name, *args, **kwargs):
-        docker_idx = metric_name.find(DOCKER_SUB_STR)
-        if docker_idx >= 0:
-            start_idx = docker_idx + DOCKER_SUB_LEN
-            dot_idx = metric_name.find('.', start_idx)
-            if dot_idx >= start_idx:
-                container_id = metric_name[start_idx:dot_idx]
-                container_name = self.containers.get(container_id)
-                if container_name:
-                    new_metric_name = ''.join((metric_name[:start_idx],
-                                               container_name,
-                                               metric_name[dot_idx:]))
-                    return super(MemoryDockerCollector, self).publish(
-                        new_metric_name, *args, **kwargs)
-                elif self.filter_existing:
-                    self.log.debug(
-                        "Skipping id '%s' (not an existing Docker container)."
-                        % container_id)
-                    return
-        return super(MemoryDockerCollector, self).publish(metric_name, *args,
+        docker_metric = _parse_metric_name(metric_name)
+        if docker_metric:
+            container_id, sep, start_idx, end_idx = docker_metric
+            container_name = self.containers.get(container_id)
+            if container_name:
+                new_metric_name = '{0}.{1}{2}'.format(
+                    metric_name[:start_idx - 1],
+                    container_name,
+                    metric_name[end_idx:])
+            elif container_name is None and self.filter_existing:
+                self.log.debug(
+                    "Skipping id '%s' (not an existing Docker container)."
+                    % container_id)
+                return
+            elif self.truncate_ids > 0:
+                new_metric_name = '{0}.{1}{2}'.format(
+                    metric_name[:start_idx - 1],
+                    container_id[:self.truncate_ids],
+                    metric_name[end_idx:])
+            elif sep != '.':
+                new_metric_name = '{0}.{1}'.format(
+                    metric_name[:start_idx - 1],
+                    metric_name[start_idx:])
+            else:
+                new_metric_name = metric_name
+        else:
+            new_metric_name = metric_name
+        return super(MemoryDockerCollector, self).publish(new_metric_name,
+                                                          *args,
                                                           **kwargs)
