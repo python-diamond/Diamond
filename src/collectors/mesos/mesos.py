8ebdc97a266df6a58976090be5b3d588aa18ef52
Michael Doyle <michaeld@twitter.com>
2015-06-19 10:36:45 -0400

    Mesos collector

---
+++ src/collectors/mesos/mesos.py
@@ -0,0 +1,73 @@
+# coding=utf-8
+
+"""
+
+Collects metrics from a mesos instance (master or slave).
+
+#### Example Configuration
+
+Typically metrics for the master are exposed on port 5050,
+while metrics for the slaves are exposed on port 5051.
+
+```
+    host = localhost
+    port = 5050
+```
+"""
+
+import diamond.collector
+import json
+import re
+import urllib2
+from pprint import pprint
+
+
+class MesosCollector(diamond.collector.Collector):
+
+    METRICS_PATH = "metrics/snapshot"
+
+    def get_default_config_help(self):
+        config_help = super(MesosCollector,
+                            self).get_default_config_help()
+        config_help.update({
+            'host': 'Hostname',
+            'port': 'Port',
+            'path': 'Metrics Path'
+        })
+        return config_help
+
+    def get_default_config(self):
+        config = super(MesosCollector, self).get_default_config()
+        config.update({
+            'host': 'localhost',
+            'port': 5050,
+            'path': 'mesos'
+        })
+        return config
+
+    def __init__(self, *args, **kwargs):
+        super(MesosCollector, self).__init__(*args, **kwargs)
+
+    def collect(self):
+        metrics = self.get_metrics()
+
+        for k, v in metrics.iteritems():
+            key = self.clean_up(k)
+            self.publish(key, v)
+
+    def get_metrics(self):
+        try:
+            url = "http://%s:%s/%s" % (self.config['host'],
+                                       self.config['port'],
+                                       self.METRICS_PATH)
+
+            response = urllib2.urlopen(url)
+            json_str = response.read()
+            return json.loads(json_str)
+        except (urllib2.HTTPError, ValueError), err:
+            self.log.error('Unable to read JSON response: %s' % err)
+            return {}
+
+    def clean_up(self, text):
+        text = re.sub('/', '.', text)
+        return text
