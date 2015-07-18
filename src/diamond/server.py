f3250534bbb20ba95e9490fe3f6b34d73cf47fb2
Jose Riguera <jriguera@gmail.com>
2015-07-18 12:51:20 +0200

    add multicollector support based on whitespace and dot split

--- src/diamond/server.py
+++ src/diamond/server.py
@@ -161,10 +161,11 @@ class Server(object):
                 )
 
                 for process_name in running_collectors - running_processes:
-                    # To handle running multiple collectors concurrently, we
-                    # split on white space and use the first word as the
-                    # collector name to spin
+                    # To handle running multiple collectors concurrently, fist
+                    # we split on white space, then on dot and use the first
+                    # word as the collector name to spin.
                     collector_name = process_name.split()[0]
+                    collector_name = collector_name.split('.', 1)[0]
 
                     if 'Collector' not in collector_name:
                         continue
