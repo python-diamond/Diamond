4f7b59a12328e0700eaf739c90218ead03db5907
Jorge Rodriguez <jorge.rodriguez@ezyinsights.com>
2015-09-25 11:28:16 +0300

    PEP8 fixes

--- src/collectors/loggly/loggly.py
+++ src/collectors/loggly/loggly.py
@@ -3,10 +3,11 @@
 """
 Collect numeric value of a field from loggly search results.
 
-This collector will retrieve the values of a configured field from loggly API and publish each one
-on the log's specified timestamp.
+This collector will retrieve the values of a configured field from loggly API
+and publish each one on the log's specified timestamp.
 
-The collector will limit the query to a timeframe equal to the collector's duration back from now.
+The collector will limit the query to a timeframe equal to the collector's
+duration back from now.
 
 #### Dependencies
 
@@ -55,10 +56,12 @@ class LogglyCollector(collector.Collector):
             LogglyCollector, self).get_default_config_help()
         config_help.update({
             'id': 'Metric identifier',
-            'field': 'Name of the field to retrieve, mutually exclusive with count',
-            'count': 'Whether to retrieve the count of matching events, mutually exclusive with field',
-            'timestamp_field': 'Name of the timestamp field. Defaults to \'timestamp\'',
-            'timestamp_format': 'Timestamp format as for strptime or \'int\' for unix time. Defaults to \'int\'',
+            'field': 'Name of the field to retrieve, required',
+            'count': 'Whether to retrieve the count of matching events',
+            'timestamp_field': 'Name of the timestamp field.'
+                               'Defaults to \'timestamp\'',
+            'timestamp_format': 'Timestamp format as for strptime or \'int\''
+                                'for unix time. Defaults to \'int\'',
             'subdomain': 'Loggly subdomain to retrieve results from',
             'query': 'Loggly search query',
             'username': 'Loggly username',
@@ -76,7 +79,8 @@ class LogglyCollector(collector.Collector):
                 'count': False,
                 'query': '*'
             }
-            [config.update({key: value}) for key, value in defaults.items() if key not in config.keys()]
+            [config.update({key: value}) for key, value in defaults.items()
+                if key not in config.keys()]
 
         super(LogglyCollector, self).process_config()
 
@@ -89,69 +93,125 @@ class LogglyCollector(collector.Collector):
                     continue
                 newconfig = configobj.ConfigObj(cfgfile)
                 self.config.merge(newconfig)
-        [self.config['metric'].pop(item, None) for item in ['path', 'extension']]
+        [self.config['metric'].pop(item, None)
+            for item in ['path', 'extension']]
 
         self.config_bunch = bunch.bunchify(self.config)
         self.metrics_config = self.config_bunch.metric
 
         for metric, config in self.metrics_config.items():
-            if 'field' in config.keys() and config.field is not 'count' and\
-                    'count' in config.keys() and not collector.str_to_bool(config.count):
+            if 'field' not in config.keys():
                 raise DiamondException(
-                    'Both field and count specified for the metric %s in file %s' % (metric, self.configfile))
-
-            if 'field' not in config.keys() and\
-                    ('count' not in config.keys() or not collector.str_to_bool(config.count)):
-                raise DiamondException(
-                    'At least one of field or count must be speficied for the metric %s in file %s' % (
-                        metric, self.configfile))
+                    'field must be specified for the metric %s in file %s' %
+                    (metric, self.configfile))
 
             set_defaults(config)
 
-            config.update({'search_URI': 'https://{0.subdomain}.loggly.com/apiv2/search?'
-                                         'q={0.query}&from=-{1.interval}s&until=now'.format(config, self.config_bunch),
-                           'events_URI': 'https://{0.subdomain}.loggly.com/apiv2/events?rsid=%s'.format(config)})
+            config.update({'search_URI': 'https://{0.subdomain}.loggly.com'
+                                         '/apiv2/search?q={0.query}&from='
+                                         '-{1.interval}s&until=now'.format(
+                                            config, self.config_bunch),
+                           'events_URI': 'https://{0.subdomain}.loggly.com'
+                                         '/apiv2/events?rsid=%s'.format(
+                                            config)})
 
     def collect(self):
         """Collect metrics from loggly."""
         def parse_timestamp(metric_config, timestamp):
             if metric_config.timestamp_format is 'int':
-                return datetime.datetime.fromtimestamp(timestamp / 10**(ceil(log10(timestamp)) - 10))
+                return datetime.datetime.fromtimestamp(
+                    timestamp / 10**(ceil(log10(timestamp)) - 10))
             else:
-                return datetime.datetime.strptime(timestamp, self.config_bunch.timestamp_format)
+                return datetime.datetime.strptime(timestamp,
+                                                  self.config_bunch
+                                                  .timestamp_format)
 
         def retrieve_results(metric_config):
+            def now_as_timefield():
+                if metric_config.timestamp_format is 'int':
+                    return time.mktime(datetime.datetime.now().timetuple())
+                else:
+                    return datetime.datetime.now().strftime(
+                        metric_config.timestamp_format)
+
+            def log_error_and_continue(code):
+                known_errors = {
+                    400: self.log.error('Loggly returned error 400:'
+                                        'Bad Request'),
+                    401: self.log.error('Loggly returned error 401:'
+                                        'Unauthorized'),
+                    403: self.log.error('Loggly returned error 403:'
+                                        'Forbidden'),
+                    404: self.log.error('Loggly returned error 404:'
+                                        'Not Found'),
+                    409: self.log.error('Loggly returned error 409:'
+                                        'Duplicate'),
+                    410: self.log.error('Loggly returned error 410:'
+                                        'Gone'),
+                    500: self.log.error('Loggly returned error 500:'
+                                        'Internal Server Error'),
+                    501: self.log.error('Loggly returned error 501:'
+                                        'Not Implemented'),
+                    503: self.log.error('Loggly returned error 503:'
+                                        'Throttled'),
+                    504: self.log.error('Loggly returned error 504:'
+                                        'Gateway Timeout')
+                }
+                try:
+                    known_errors[code]
+                except KeyError:
+                    self.log.error('Loggly returned unrecognized error code %s'
+                                   % code)
+                finally:
+                    return []
+
             search_response = requests.get(
                 metric_config.search_URI,
                 auth=(metric_config.username, metric_config.password))
-            events = requests.get(
-                metric_config.events_URI % search_response.json()['rsid']['id'],
-                auth=(metric_config.username, metric_config.password))
+            if search_response.status_code is 200:
+                events = requests.get(
+                    metric_config.events_URI % search_response
+                    .json()['rsid']['id'],
+                    auth=(metric_config.username, metric_config.password))
 
-            if metric_config.count:
-                return [bunch.bunchify({'timestamp': datetime.datetime.now(), 'value': events.json()['total_events']})]
+                if events.status_code is 200:
+                    bare_metrics = [(metric_config.field,
+                                     event[metric_config.timestamp_field],
+                                     multi_getattr(bunch.bunchify(event).event,
+                                                   metric_config.field))
+                                    for event in events.json()['events']]
+                    if metric_config.count:
+                        bare_metrics.append(('count', now_as_timefield(),
+                                            events.json()['total_events']))
+                else:
+                    return log_error_and_continue(events.status_code)
             else:
-                bare_metrics = [(event[metric_config.timestamp_field], multi_getattr(bunch.bunchify(event).event,
-                                metric_config.field)) for event in events.json()['events']]
-                return map(lambda bm: bunch.bunchify({'timestamp': parse_timestamp(metric_config, bm[0]),
-                                                      'value': float(bm[1])}),
-                           bare_metrics)
+                return log_error_and_continue(search_response.status_code)
+            return map(lambda bm: bunch.bunchify({
+                        'field': bm[0],
+                        'timestamp': parse_timestamp(metric_config, bm[1]),
+                        'value': float(bm[2])
+                       }),
+                       bare_metrics)
 
         def parallel_retrieval(results, metric, config):
-            results.update({metric: bunch.bunchify({'results': retrieve_results(config)})})
-            results.get(metric).update(bunch.bunchify({'field': config.field}))
+            results.update({metric: bunch.bunchify(
+                {'results': retrieve_results(config)})})
 
         results = bunch.Bunch()
-        with concurrent.futures.ThreadPoolExecutor(len(self.metrics_config.items())) as executor:
-            futures = [executor.submit(parallel_retrieval, results, metric, config)
+        with concurrent.futures.ThreadPoolExecutor(
+                len(self.metrics_config.items())) as executor:
+            futures = [executor.submit(
+                            parallel_retrieval, results, metric, config)
                        for metric, config in self.metrics_config.items()]
             concurrent.futures.wait(futures)
 
         for metric, results in results.items():
             for result in results.results:
-                self.publish('.'.join([metric, results.field]),
+                self.publish('.'.join([metric, result.field]),
                              result.value,
-                             timestamp=time.mktime(result.timestamp.timetuple()))
+                             timestamp=time.mktime(
+                                            result.timestamp.timetuple()))
 
     def publish(self, name, value, timestamp=None, raw_value=None,
                 precision=0, metric_type='GAUGE', instance=None):
@@ -173,9 +233,10 @@ class LogglyCollector(collector.Collector):
 
         # Create Metric
         try:
-            metric = Metric(path, value, raw_value=raw_value, timestamp=timestamp,
-                            precision=precision, host=self.get_hostname(),
-                            metric_type=metric_type, ttl=ttl)
+            metric = Metric(path, value, raw_value=raw_value,
+                            timestamp=timestamp, precision=precision,
+                            host=self.get_hostname(), metric_type=metric_type,
+                            ttl=ttl)
         except DiamondException:
             self.log.error(('Error when creating new Metric: path=%r, '
                             'value=%r'), path, value)
