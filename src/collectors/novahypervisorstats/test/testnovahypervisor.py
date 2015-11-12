0cb922b67fbf3bd6f7a5c113c6993c1fb2e34bb1
Stuart Fox <sfox@xmatters.com>
2015-11-11 20:42:31 -0800

    Start getting tests ready, its a new mock adventure!

--- src/collectors/novahypervisorstats/test/testnovahypervisor.py
+++ src/collectors/novahypervisorstats/test/testnovahypervisor.py
@@ -4,7 +4,10 @@
 
 from test import CollectorTestCase
 from test import get_collector_config
+from mock import Mock
+from mock import patch
 from test import unittest
+from diamond.collector import Collector
 
 from novahypervisorstats import NovaHypervisorStatsCollector
 
@@ -22,6 +25,27 @@ class TestNovaHypervisorStatsCollector(CollectorTestCase):
 
     def test_import(self):
         self.assertTrue(NovaHypervisorStatsCollector)
+
+    @patch.object(Collector, 'publish')
+    def test_should_work_with_real_data(self, publish_mock):
+        instance = novaclient.client.HTTPClient(user='user',
+                                                password='password',
+                                                projectid='project',
+                                                timeout=2,
+                                                auth_url="http://www.blah.com")
+        self.assertEqual(2, instance.timeout)
+        mock_request = mock.Mock()
+        mock_request.return_value = requests.Response()
+        mock_request.return_value.status_code = 200
+        mock_request.return_value.headers = {
+            'x-server-management-url': 'blah.com',
+            'x-auth-token': 'blah',
+        }
+        with mock.patch('requests.request', mock_request):
+            instance.authenticate()
+            requests.request.assert_called_with(
+                mock.ANY, mock.ANY, timeout=2, headers=mock.ANY,
+                verify=mock.ANY)
 
 ##########################################################################
 if __name__ == "__main__":
