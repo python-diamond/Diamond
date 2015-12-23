6dd40eca94d58814b3df7a8f5863d2b7fe7c60da
Jumping Qu <jumping@users.noreply.github.com>
2015-12-23 17:28:07 +0800

    Support amzn image
    split the pull https://github.com/python-diamond/Diamond/pull/358

--- setup.py
+++ setup.py
@@ -44,6 +44,9 @@ else:
 
     distro = platform.dist()[0]
     distro_major_version = platform.dist()[1].split('.')[0]
+    if not distro:
+        if 'amzn' in platform.uname()[2]:
+            distro = 'centos'
 
     if running_under_virtualenv():
         data_files.append(('etc/diamond',
