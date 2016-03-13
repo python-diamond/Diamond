2d29aff8c0fec7c05b63bdd976458ef8656c05cf
Keith Baker <kbaker@alumni.ithaca.edu>
2016-03-13 18:58:49 -0400

    typo

--- src/collectors/snmp/snmp.py
+++ src/collectors/snmp/snmp.py
@@ -120,7 +120,7 @@ class SNMPCollector(diamond.collector.Collector):
         host = socket.gethostbyname(host)
 
         # Assemble SNMP Auth Data
-        snmpAuthData = cmdgen.CommunityData('agent', community, mpmodel = 0)
+        snmpAuthData = cmdgen.CommunityData('agent', community, mpModel = 0)
 
         # Assemble SNMP Transport Data
         snmpTransportData = cmdgen.UdpTransportTarget(
