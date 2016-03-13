381cd74f55757d2cbf42a028af3938fca077a571
Keith Baker <kbaker@alumni.ithaca.edu>
2016-03-13 18:52:28 -0400

    force snmp v1...  should be a better way to fix this

--- src/collectors/snmp/snmp.py
+++ src/collectors/snmp/snmp.py
@@ -86,7 +86,7 @@ class SNMPCollector(diamond.collector.Collector):
         host = socket.gethostbyname(host)
 
         # Assemble SNMP Auth Data
-        snmpAuthData = cmdgen.CommunityData('agent', community)
+        snmpAuthData = cmdgen.CommunityData('agent', community, mpModel = 0)
 
         # Assemble SNMP Transport Data
         snmpTransportData = cmdgen.UdpTransportTarget(
@@ -120,7 +120,7 @@ class SNMPCollector(diamond.collector.Collector):
         host = socket.gethostbyname(host)
 
         # Assemble SNMP Auth Data
-        snmpAuthData = cmdgen.CommunityData('agent', community)
+        snmpAuthData = cmdgen.CommunityData('agent', community, mpmodel = 0)
 
         # Assemble SNMP Transport Data
         snmpTransportData = cmdgen.UdpTransportTarget(
