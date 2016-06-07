e6d98ed0adaaecfe1adf0aef710c814c63329a92
Oscar Wahlberg <oscar.wahlberg@gmail.com>
2016-06-06 22:03:17 -0700

    PEP8 formatting errors.

--- src/collectors/bcache/bcache.py
+++ src/collectors/bcache/bcache.py
@@ -29,14 +29,14 @@ class BcacheCollector(diamond.collector.Collector):
         })
         return config
 
-    def file_to_lines(self,fname):
+    def file_to_lines(self, fname):
         try:
             with open(fname, "r") as fd:
                 return fd.readlines()
         except:
             return []
 
-    def file_to_line(self,fname):
+    def file_to_line(self, fname):
         ret = self.file_to_lines(fname)
         if ret:
             return ret[0].strip()
@@ -56,28 +56,32 @@ class BcacheCollector(diamond.collector.Collector):
 
         return uuids
 
-    def map_uuid_to_bcache(self,uuid):
+    def map_uuid_to_bcache(self, uuid):
         devices = []
         for obj in os.listdir(os.path.join(SYSFS_BCACHE_PATH, uuid)):
             if obj.startswith('bdev'):
-               devices.append(os.path.basename(os.readlink(os.path.join(SYSFS_BCACHE_PATH, uuid, obj, 'dev'))))
+                devices.append(os.path.basename(os.readlink(
+                  os.path.join(SYSFS_BCACHE_PATH, uuid, obj, 'dev'))))
         return devices
 
     def get_cache_result(self, uuid, stat):
         value = 0
         for obj in os.listdir(os.path.join(SYSFS_BCACHE_PATH, uuid)):
             if obj.startswith('bdev'):
-                value = int(self.file_to_line('%s/%s/%s/stats_five_minute/cache_%s' %
-                                    (SYSFS_BCACHE_PATH, uuid, obj, stat)))
+                value = int(self.file_to_line(
+                    '%s/%s/%s/stats_five_minute/cache_%s' %
+                    (SYSFS_BCACHE_PATH, uuid, obj, stat)))
         return value
 
     def get_cache_ratio(self, uuid, time):
         for obj in os.listdir(os.path.join(SYSFS_BCACHE_PATH, uuid)):
             if obj.startswith('bdev'):
-                hits = float(self.file_to_line('%s/%s/%s/stats_%s/cache_hits' %
-                                        (SYSFS_BCACHE_PATH, uuid, obj, time)))
-                misses = float(self.file_to_line('%s/%s/%s/stats_%s/cache_misses' %
-                                          (SYSFS_BCACHE_PATH, uuid, obj, time)))
+                hits = float(self.file_to_line(
+                    '%s/%s/%s/stats_%s/cache_hits' %
+                    (SYSFS_BCACHE_PATH, uuid, obj, time)))
+                misses = float(self.file_to_line(
+                    '%s/%s/%s/stats_%s/cache_misses' %
+                    (SYSFS_BCACHE_PATH, uuid, obj, time)))
                 if (hits + misses) == 0:
                     return 100
                 return hits / (hits + misses) * 100
@@ -88,13 +92,14 @@ class BcacheCollector(diamond.collector.Collector):
         Overrides the Collector.collect method
         """
         for uuid in self.bcache_uuids():
-            for device in self.map_uuid_to_bcache (uuid):
+            for device in self.map_uuid_to_bcache(uuid):
                 for t in ['five_minute', 'hour', 'day', 'total']:
                     cache_ratio = self.get_cache_ratio(uuid, t)
                     metric_name = "%s.cache_ratio-%s" % (device, t)
                     self.publish(metric_name, cache_ratio)
 
-                for c in ['bypass_hits', 'bypass_misses', 'hits', 'miss_collisions', 'misses', 'readaheads']:
+                for c in ['bypass_hits', 'bypass_misses', 'hits',
+                          'miss_collisions', 'misses', 'readaheads']:
                     cache_result = self.get_cache_result(uuid, c)
                     metric_name = "%s.cache_results-%s" % (device, c)
                     self.publish(metric_name, cache_result)
