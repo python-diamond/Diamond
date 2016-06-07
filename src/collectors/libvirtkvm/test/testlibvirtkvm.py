6aacb20110981419c478576db650ea2c90c48f8d
biancalana <ale@Ale-MacBook-Pro.local>
2016-06-07 14:04:11 -0300

    initial tests

--- src/collectors/libvirtkvm/test/testlibvirtkvm.py
+++ src/collectors/libvirtkvm/test/testlibvirtkvm.py
@@ -21,6 +21,40 @@ class TestLibvirtKVMCollector(CollectorTestCase):
     def test_import(self):
         self.assertTrue(LibvirtKVMCollector)
 
+    @patch.object(Collector, 'publish')
+
+    def test_should_work_with_real_data(self, publish_mock, conn_mock):
+        LibvirtKVMCollector.conn = Mock()
+        conn = True
+        listDomainsID = [ 1 ]
+        lookupByID = True
+
+        LibvirtKVMCollector.conn_mock = Mock()
+
+        dom.getCPUStats
+        dom.getCPUStats
+
+        LibvirtKVMCollector.dom. = self.getFixturePath('proc_meminfo')
+        self.collector.collect()
+
+        metrics = {
+            'MemTotal': 49554212,
+            'MemFree': 35194496,
+            'Buffers': 1526304,
+            'Cached': 10726736,
+            'Active': 10022168,
+            'Dirty': 24748,
+            'Inactive': 2524928,
+            'Shmem': 276,
+            'SwapTotal': 262143996,
+            'SwapFree': 262143996,
+            'SwapCached': 0,
+            'VmallocTotal': 34359738367,
+            'VmallocUsed': 445452,
+            'VmallocChunk': 34311049240
+        }
+
+
 ###############################################################################
 if __name__ == "__main__":
     unittest.main()
