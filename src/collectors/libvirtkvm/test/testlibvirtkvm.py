1078a788947640c1337e2ed930ed29491066e67d
Alexandre Biancalana <abiancalana@uoldiveo.com>
2016-06-07 19:18:48 -0300

    - added some mocking

--- src/collectors/libvirtkvm/test/testlibvirtkvm.py
+++ src/collectors/libvirtkvm/test/testlibvirtkvm.py
@@ -5,9 +5,10 @@
 from test import CollectorTestCase
 from test import get_collector_config
 from test import unittest
+from mock import Mock
+from mock import patch
 
 from libvirtkvm import LibvirtKVMCollector
-
 
 ###############################################################################
 
@@ -21,20 +22,27 @@ class TestLibvirtKVMCollector(CollectorTestCase):
     def test_import(self):
         self.assertTrue(LibvirtKVMCollector)
 
+    @patch('libvirt')
     @patch.object(Collector, 'publish')
 
-    def test_should_work_with_real_data(self, publish_mock, conn_mock):
-        LibvirtKVMCollector.conn = Mock()
-        conn = True
+    def test_should_work_with_real_data(self, publish_mock, libvirt_mock):
+        libvirt = Mock()
+        conn_mock = Mock()
+        dom_mock = Mock()
+
         listDomainsID = [ 1 ]
-        lookupByID = True
 
-        LibvirtKVMCollector.conn_mock = Mock()
+        libvirt_mock.return_value = libvirt
+        libvirt.openReadOnly.return_value = conn_mock
+        conn_mock.listDomainsID.return_value = listDomainsID
+        conn_mock.lookupByID.return_value = dom_mock
 
-        dom.getCPUStats
-        dom.getCPUStats
+        dom_mock.UUIDString.return_value = "uuid"
 
-        LibvirtKVMCollector.dom. = self.getFixturePath('proc_meminfo')
+        dom_mock.getCPUStats.return_value = [{'cpu_time': 30375130070L,
+                                                'system_time': 6980000000L,
+                                                'user_time': 3120000000L}]
+
         self.collector.collect()
 
         metrics = {
