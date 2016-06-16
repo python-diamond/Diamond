41ed71f8fff0a7753906389c12490af3139594d6
cszerzo <csabiftw@gmail.com>
2016-06-16 14:47:14 +0200

    Fix docker collector exception handling

--- src/collectors/docker_collector/docker_collector.py
+++ src/collectors/docker_collector/docker_collector.py
@@ -61,42 +61,38 @@ class DockerCollector(diamond.collector.Collector):
         if docker is None:
             self.log.error('Unable to import docker')
 
-        try:
-            # Collect info
-            results = {}
-            client = docker.Client(version='auto')
+        # Collect info
+        results = {}
+        client = docker.Client(version='auto')
 
-            # Top level stats
-            running_containers = client.containers()
-            results['containers_running_count'] = (
-                len(running_containers), 'GAUGE')
+        # Top level stats
+        running_containers = client.containers()
+        results['containers_running_count'] = (
+            len(running_containers), 'GAUGE')
 
-            all_containers = client.containers(all=True)
-            results['containers_stopped_count'] = (
-                len(all_containers) - len(running_containers), 'GAUGE')
+        all_containers = client.containers(all=True)
+        results['containers_stopped_count'] = (
+            len(all_containers) - len(running_containers), 'GAUGE')
 
-            images_count = len(set(client.images(quiet=True)))
-            results['images_count'] = (images_count, 'GAUGE')
+        images_count = len(set(client.images(quiet=True)))
+        results['images_count'] = (images_count, 'GAUGE')
 
-            dangling_images_count = len(set(client.images(
-                quiet=True, all=True, filters={'dangling': True})))
-            results['images_dangling_count'] = (dangling_images_count, 'GAUGE')
+        dangling_images_count = len(set(client.images(
+            quiet=True, all=True, filters={'dangling': True})))
+        results['images_dangling_count'] = (dangling_images_count, 'GAUGE')
 
-            # Collect memory and cpu stats
-            for container in running_containers:
-                name = "containers." + "".join(container['Names'][0][1:])
-                s = client.stats(container["Id"])
-                stat = json.loads(s.next())
-                for path in self.METRICS:
-                    val = self.get_value(path, stat)
-                    if val is not None:
-                        metric_key = ".".join([name, self.METRICS.get(path)])
-                        results[metric_key] = (val, 'GAUGE')
-                s.close()
+        # Collect memory and cpu stats
+        for container in running_containers:
+            name = "containers." + "".join(container['Names'][0][1:])
+            s = client.stats(container["Id"])
+            stat = json.loads(s.next())
+            for path in self.METRICS:
+                val = self.get_value(path, stat)
+                if val is not None:
+                    metric_key = ".".join([name, self.METRICS.get(path)])
+                    results[metric_key] = (val, 'GAUGE')
+            s.close()
 
-            for name in sorted(results.keys()):
-                (value, metric_type) = results[name]
-                self.publish(name, value, metric_type=metric_type)
-
-        except Exception, e:
-            self.log.error(e, exc_info=True)
+        for name in sorted(results.keys()):
+            (value, metric_type) = results[name]
+            self.publish(name, value, metric_type=metric_type)
