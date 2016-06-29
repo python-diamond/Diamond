9bd207243a426c7024f8c6323df4045ccb1ca9ed
shawnbutts <shawnbutts@users.noreply.github.com>
2016-06-29 14:32:13 -0400

    fix docker collector
    work around for https://github.com/docker/docker/issues/22375

--- src/collectors/netuitivedocker/netuitivedocker.py
+++ src/collectors/netuitivedocker/netuitivedocker.py
@@ -51,32 +51,37 @@ class NetuitiveDockerCollector(diamond.collector.Collector):
             # memory metrics
             self.memory = self.flatten_dict(metrics['memory_stats'])
             for key, value in self.memory.items():
-                metric_name = name + ".memory." + key
-                self.publish_gauge(metric_name, value)
+                if value != None:
+                    metric_name = name + ".memory." + key
+                    self.publish_gauge(metric_name, value)
             # cpu metrics
             self.cpu = self.flatten_dict(metrics['cpu_stats'])
             for key, value in self.cpu.items():
-                # percpu_usage is a list, we'll deal with it after
-                if type(value) == int:
-                    metric_name = name + ".cpu." + key
-                    self.publish_counter(metric_name, value)
-                # dealing with percpu_usage
-                if type(value) == list:
-                    self.length = len(value)
-                    for i in range(self.length):
-                        self.value = value
-                        self.metric_name = name + ".cpu." + key + str(i)
-                        self.publish_counter(self.metric_name, self.value[i])
+                if value != None:
+                    # percpu_usage is a list, we'll deal with it after
+                    if type(value) == int:
+                        metric_name = name + ".cpu." + key
+                        self.publish_counter(metric_name, value)
+                    # dealing with percpu_usage
+                    if type(value) == list:
+                        self.length = len(value)
+                        for i in range(self.length):
+                            self.value = value
+                            self.metric_name = name + ".cpu." + key + str(i)
+                            self.publish_counter(
+                                self.metric_name, self.value[i])
             # network metrics
             self.network = self.flatten_dict(metrics['network'])
             for key, value in self.network.items():
-                metric_name = name + ".network." + key
-                self.publish_counter(metric_name, value)
+                if value != None:
+                    metric_name = name + ".network." + key
+                    self.publish_counter(metric_name, value)
             # blkio metrics
             self.blkio = self.flatten_dict(metrics['blkio_stats'])
             for key, value in self.blkio.items():
-                metric_name = name + ".blkio." + key
-                self.publish_counter(metric_name, value)
+                if value != None:
+                    metric_name = name + ".blkio." + key
+                    self.publish_counter(metric_name, value)
 
         cc = docker.Client(
             base_url='unix://var/run/docker.sock', version='auto')
