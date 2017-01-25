60bcfa9ac681e9fc9bf908afd3b7cbb89662d68e
GDR! <gdr@gdr.name>
2017-01-25 20:59:51 +0100

    More pep8

--- src/collectors/netstat/netstat.py
+++ src/collectors/netstat/netstat.py
@@ -22,18 +22,18 @@ import glob
 class NetstatCollector(diamond.collector.Collector):
     PROC_TCP = "/proc/net/tcp"
     STATE = {
-            '01': 'ESTABLISHED',
-            '02': 'SYN_SENT',
-            '03': 'SYN_RECV',
-            '04': 'FIN_WAIT1',
-            '05': 'FIN_WAIT2',
-            '06': 'TIME_WAIT',
-            '07': 'CLOSE',
-            '08': 'CLOSE_WAIT',
-            '09': 'LAST_ACK',
-            '0A': 'LISTEN',
-            '0B': 'CLOSING'
-            }
+        '01': 'ESTABLISHED',
+        '02': 'SYN_SENT',
+        '03': 'SYN_RECV',
+        '04': 'FIN_WAIT1',
+        '05': 'FIN_WAIT2',
+        '06': 'TIME_WAIT',
+        '07': 'CLOSE',
+        '08': 'CLOSE_WAIT',
+        '09': 'LAST_ACK',
+        '0A': 'LISTEN',
+        '0B': 'CLOSING'
+    }
 
     def get_default_config(self):
         """
