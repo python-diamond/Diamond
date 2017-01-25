9544a95fe19f65eb69fd38cb054f7c55f56caa76
GDR! <gdr@gdr.name>
2017-01-25 19:19:30 +0100

    PEP8 in netstat.py

--- src/collectors/netstat/netstat.py
+++ src/collectors/netstat/netstat.py
@@ -18,20 +18,21 @@ import os
 import re
 import glob
 
+
 class NetstatCollector(diamond.collector.Collector):
     PROC_TCP = "/proc/net/tcp"
     STATE = {
-            '01':'ESTABLISHED',
-            '02':'SYN_SENT',
-            '03':'SYN_RECV',
-            '04':'FIN_WAIT1',
-            '05':'FIN_WAIT2',
-            '06':'TIME_WAIT',
-            '07':'CLOSE',
-            '08':'CLOSE_WAIT',
-            '09':'LAST_ACK',
-            '0A':'LISTEN',
-            '0B':'CLOSING'
+            '01': 'ESTABLISHED',
+            '02': 'SYN_SENT',
+            '03': 'SYN_RECV',
+            '04': 'FIN_WAIT1',
+            '05': 'FIN_WAIT2',
+            '06': 'TIME_WAIT',
+            '07': 'CLOSE',
+            '08': 'CLOSE_WAIT',
+            '09': 'LAST_ACK',
+            '0A': 'LISTEN',
+            '0B': 'CLOSING'
             }
 
     def get_default_config(self):
@@ -55,7 +56,7 @@ class NetstatCollector(diamond.collector.Collector):
         for line in content:
             line_array = self._remove_empty(line.split(' '))
             state = self.STATE[line_array[3]]
-            
+
             result[state] += 1
 
         for state in result:
@@ -64,25 +65,28 @@ class NetstatCollector(diamond.collector.Collector):
     @staticmethod
     def _load():
         ''' Read the table of tcp connections & remove header  '''
-        with open(NetstatCollector.PROC_TCP,'r') as f:
+        with open(NetstatCollector.PROC_TCP, 'r') as f:
             content = f.readlines()
             content.pop(0)
         return content
 
     @staticmethod
     def _hex2dec(s):
-        return str(int(s,16))
+        return str(int(s, 16))
 
     @staticmethod
     def _ip(s):
-        ip = [(NetstatCollector._hex2dec(s[6:8])),(NetstatCollector._hex2dec(s[4:6])),(NetstatCollector._hex2dec(s[2:4])),(NetstatCollector._hex2dec(s[0:2]))]
+        ip = [(NetstatCollector._hex2dec(s[6:8])),
+              (NetstatCollector._hex2dec(s[4:6])),
+              (NetstatCollector._hex2dec(s[2:4])),
+              (NetstatCollector._hex2dec(s[0:2]))]
         return '.'.join(ip)
 
     @staticmethod
     def _remove_empty(array):
-        return [x for x in array if x !='']
+        return [x for x in array if x != '']
 
     @staticmethod
     def _convert_ip_port(array):
-        host,port = array.split(':')
-        return NetstatCollector._ip(host),NetstatCollector._hex2dec(port)
+        host, port = array.split(':')
+        return NetstatCollector._ip(host), NetstatCollector._hex2dec(port)
