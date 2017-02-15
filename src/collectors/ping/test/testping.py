02e0090c17585ecd03c17ab8db7a5cd0d680bf7a
Arnout Boks <arnoutboks@gmail.com>
2017-02-15 08:46:05 +0100

    Also update tests that generate examples for #218

--- src/collectors/ping/test/testping.py
+++ src/collectors/ping/test/testping.py
@@ -48,18 +48,44 @@ class TestPingCollector(CollectorTestCase):
     @patch('os.access', Mock(return_value=True))
     @patch.object(Collector, 'publish')
     def test_should_work_with_real_data_host_gentoo(self, publish_mock):
-        patch_communicate = patch(
-            'subprocess.Popen.communicate',
-            Mock(return_value=(
-                self.getFixture('host_gentoo').getvalue(),
-                '')))
+        config = get_collector_config('PingCollector', {
+            'interval': 10,
+            'target_a': 'localhost',
+            'target_b': 'www.example.org',
+            'target_c': '192.168.0.1',
+            'bin': 'true'
+        })
 
-        patch_communicate.start()
-        self.collector.collect()
-        patch_communicate.stop()
+        collector = PingCollector(config, None)
+
+        def popen_se(command, **kwargs):
+            popen_mock = Mock()
+            if command[-1] == 'localhost':
+                popen_mock.communicate = Mock(return_value=(
+                    self.getFixture('host_gentoo').getvalue(),
+                    ''))
+            elif command[-1] == 'www.example.org':
+                popen_mock.communicate = Mock(return_value=(
+                    self.getFixture('host_gentoo2').getvalue(),
+                    ''))
+            elif command[-1] == '192.168.0.1':
+                popen_mock.communicate = Mock(return_value=(
+                    self.getFixture('host_gentoo3').getvalue(),
+                    ''))
+            return popen_mock
+
+        patch_popen = patch(
+            'subprocess.Popen',
+            Mock(side_effect=popen_se))
+
+        patch_popen.start()
+        collector.collect()
+        patch_popen.stop()
 
         metrics = {
-            'localhost': 11
+            'localhost': 11,
+            'www_example_org': 23,
+            '192_168_0_1': 16
         }
 
         self.setDocExample(collector=self.collector.__class__.__name__,
