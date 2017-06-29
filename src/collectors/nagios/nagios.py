611d854574d192e3134bd88655cc0cb886376d34
William Leese <wleese@bol.com>
2017-06-29 12:01:38 +0200

    fix whitespace
    Change-Id: Ifa323bbb06bcbeb066a50bcae262fd061957295f

--- src/collectors/nagios/nagios.py
+++ src/collectors/nagios/nagios.py
@@ -71,12 +71,12 @@ class NagiosStatsCollector(diamond.collector.Collector):
     def collect(self):
         # Cannot access binary and not using sudo
         if (not os.access(self.config['bin'], os.X_OK) and
-             not str_to_bool(self.config['use_sudo'])):
+            not str_to_bool(self.config['use_sudo'])):
             return
 
         # Using sudo but cannot access it
         if (str_to_bool(self.config['use_sudo']) and
-              not os.access(self.config['sudo_cmd'], os.X_OK)):
+            not os.access(self.config['sudo_cmd'], os.X_OK)):
             return
 
         command = [self.config['bin'],
