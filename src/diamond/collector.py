3472a2c06cdd686f6a06055646ea67f6d1672c77
Thomas Röhl <thomas.roehl@fau.de>
2017-07-28 10:52:03 +0200

    Allow setting a timestamp when publishing a metric

--- src/diamond/collector.py
+++ src/diamond/collector.py
@@ -371,7 +371,7 @@ class Collector(object):
         raise NotImplementedError()
 
     def publish(self, name, value, raw_value=None, precision=0,
-                metric_type='GAUGE', instance=None):
+                metric_type='GAUGE', instance=None, timestamp=None):
         """
         Publish a metric with the given name
         """
@@ -392,7 +392,7 @@ class Collector(object):
 
         # Create Metric
         try:
-            metric = Metric(path, value, raw_value=raw_value, timestamp=None,
+            metric = Metric(path, value, raw_value=raw_value, timestamp=timestamp,
                             precision=precision, host=self.get_hostname(),
                             metric_type=metric_type, ttl=ttl)
         except DiamondException:
