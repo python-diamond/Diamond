2c75ab27bf2fb66daf44393356ff32d54c518812
David Stapleton <dstaplet@mail.eng.vyatta.net>
2018-02-28 21:08:30 +0000

    Minor scheduler log improvements

--- src/diamond/utils/scheduler.py
+++ src/diamond/utils/scheduler.py
@@ -36,14 +36,13 @@ def collector_process(collector, metric_queue, log):
 
     interval = float(collector.config['interval'])
 
-    log.debug('Starting')
-
-    log.debug('Interval: %s seconds', interval)
-
     # Validate the interval
     if interval <= 0:
-        log.critical('interval of %s is not valid!', interval)
+        log.critical('Interval of %s is not valid!', interval)
         sys.exit(1)
+
+    log.debug('Starting %s', collector.name)
+    log.debug('Interval: %s seconds', interval)
 
     # Initially set stagger_offset to 0. If stagger_collection is enabled in
     # the config, this value will soon be overridden.
@@ -103,7 +102,7 @@ def collector_process(collector, metric_queue, log):
             # us and end up with half a loaded config
             signal.alarm(0)
 
-            log.info('Reloading config reload due to HUP')
+            log.info('Reloading config due to SIGHUP')
             collector.load_config()
             log.info('Config reloaded')
 
