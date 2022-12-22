80a966a35942d85601bb12dbef9e29b81c193d51
Chen Dong <cdong@mediamath.com>
2022-12-22 15:52:56 -0500

    add an option to manually set value sent to StatsD instead of using the difference by default

--- src/diamond/handler/stats_d.py
+++ src/diamond/handler/stats_d.py
@@ -132,6 +132,9 @@ class StatsdHandler(Handler):
                 value = metric.raw_value
                 if metric.path in self.old_values:
                     value = value - self.old_values[metric.path]
+                # provide an option for caller to manually set the value
+                elif metric.value:
+                    value = metric.value
                 self.old_values[metric.path] = metric.raw_value
 
                 if hasattr(statsd, 'StatsClient'):
